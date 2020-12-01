5285caa56ad95b3c3f98e785815bcbeed050c99a
Léo Germond <germond@adacore.com>
2020-12-01 09:55:08 +0100

    Add windows 32bit, fix CE 2020

--- courses/fundamentals_of_ada/labs/solar_system/start_windows_x86.bat
+++ courses/fundamentals_of_ada/labs/solar_system/start_windows_x86.bat
@@ -1,9 +1,9 @@
 set LOCAL=%~dp0
 set INSTALL=E:\install\GNAT
-set PATH=%INSTALL%\gnat-community-2020-x86_64-windows\bin;%PATH%
-set PATH=%LOCAL%\libs64;%PATH%
+set PATH=%INSTALL%\gnat-community-2020-x86-windows\bin;%PATH%
+set PATH=%LOCAL%\libs;%PATH%
 set ADA_PROJECT_PATH=%LOCAL%\game_support;%LOCAL%\gnat_sdl;
-set LIBRARY_PATH=%LOCAL%\libs64;%LIBRARY_PATH%
+set LIBRARY_PATH=%LOCAL%\libs;%LIBRARY_PATH%
 set GNAT_STUDIO_HOME=%LOCAL%
 set HOST=Windows
 @echo off
@@ -11,7 +11,7 @@ echo -
 echo -
 echo ********************************
 echo **                            **
-echo ** 64 bits environment ready  **
+echo ** 32 bits environment ready  **
 echo **                            **
 echo ********************************
 echo - 
