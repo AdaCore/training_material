11da0e3980893d3ff8f01c3ebdd3dc459be5d9aa
Léo Germond <germond@adacore.com>
2021-01-26 11:08:49 +0100

    Lab 030: answer: 10cm in km

--- courses/fundamentals_of_ada/labs/radar/030_basic_types/answers/main.adb
+++ courses/fundamentals_of_ada/labs/radar/030_basic_types/answers/main.adb
@@ -16,7 +16,7 @@ procedure Main is
 
    -- Define a subtype Object_Distance_Km_T as a Float with values
    -- between 10cm and 100km
-   subtype Object_Distance_Km_T is Float range 0.01 .. 100.0;
+   subtype Object_Distance_Km_T is Float range 0.000_01 .. 100.0;
 
    -- Define a subtype Speed_Kph_T that is a Float between 0 and 50 km/h
    subtype Speed_Kph_T is Float range 0.0 .. 50.0;
