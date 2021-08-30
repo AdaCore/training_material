a52486c4e136eeb9cd47f0580c235c042d30d405
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2021-08-30 14:52:23 -0400

    Start creating lab prompts (source code prompt and GPR file)

---
+++ courses/fundamentals_of_ada/labs/prompts/030_basic_types/main.adb
@@ -0,0 +1,40 @@
+with Ada.Text_IO; use Ada.Text_IO;
+procedure Main is
+
+   --  type Number_Of_Tests_T is ?
+   --  type Test_Score_Total_T is ?
+   --  Number_Of_Tests  : Number_Of_Tests_T;
+   --  Test_Score_Total : Test_Score_Total_T;
+
+   --  type Degrees_T is ?
+   --  Angle : Degrees_T;
+
+   --  type Cymk_T is ?
+   --  Color : Cymk_T;
+
+begin
+
+   Put_Line ("Basic Types");
+
+   -- assignment
+   --  Number_Of_Tests  := ?
+   --  Test_Score_Total := ?
+   --  Angle            := ?
+   --  Color            := ?
+
+   --  Put_Line (Number_Of_Tests_T'Image(Number_Of_Tests));
+   --  Put_Line (Test_Score_Total_T'Image(Test_Score_Total));
+   --  Put_Line (Angle_T'Image(Angle));
+   --  Put_Line (Color_T'Image(Color));
+
+   -- operations / attributes
+   --  Test_Score_Total := ? -- Divide total score by number of tests
+   --  Angle            := ? -- Add 359 to the initial value
+   --  Color            := ? -- Set to value prior to last possible value
+
+   --  Put_Line (Test_Score_Total'Image);
+   --  Put_Line (Angle'Image);
+   --  Put_Line (Color'Image);
+
+end Main;
+--Implementation
---
+++ courses/fundamentals_of_ada/labs/prompts/040_statements/main.adb
@@ -0,0 +1,31 @@
+with Ada.Text_IO; use Ada.Text_IO;
+procedure Main is
+   type Days_Of_Week_T is
+     (Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday);
+   type Hours_Worked is digits 6;
+
+   Total_Worked : Hours_Worked := 0.0;
+   Hours_Today  : Hours_Worked;
+   Overtime     : Hours_Worked;
+begin
+   -- For each day in the Days_Of_Week_T loop
+      -- Print prompt indicating which day we're asking for
+      -- Loop "forever"
+         Hours_Today := Hours_Worked'Value (Get_Line);
+         -- exit the main loop if hours worked < 0
+         if Hours_Today > 24.0 then
+            Put_Line ("I don't believe you");
+         else
+            -- exit the input loop if a reasonable number is reached
+            null;
+         end if;
+      -- if hours worked > 8 then
+         Overtime := Hours_Today - 8.0;
+         Hours_Today := Hours_Today + 1.5 * Overtime;
+      -- Calculate actual hours paid for based on day of the week
+         -- Monday - Friday, just add hours worked
+         -- Saturday - hours worked * 1.5
+         -- Sunday - hours worked * 2
+
+   Put_Line (Hours_Worked'Image(Total_Worked));
+end Main;
