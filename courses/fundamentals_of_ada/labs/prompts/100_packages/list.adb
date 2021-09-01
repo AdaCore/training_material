8bc1a2297d116c2362c7b6ef44360d44df205612
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2021-09-01 10:00:10 -0400

    Three more lab prompts

---
+++ courses/fundamentals_of_ada/labs/prompts/100_packages/list.adb
@@ -0,0 +1,16 @@
+
+with Ada.Text_IO; use Ada.Text_IO;
+with Constants;
+package body List is
+
+   -- Create a list whose maximum size is specified in Constants package
+
+   -- Create subprogram to add to list (print a message if the list is full)
+
+   -- Create subprogram to remove an item from the list
+
+   -- Create subprogram to print the list
+
+   -- Create a function to return the current length of the list
+
+end List;
---
+++ courses/fundamentals_of_ada/labs/prompts/110_private_types/main.adb
@@ -0,0 +1,29 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Colors;
+with Flags;
+with Input;
+procedure Main is
+   Map : Flags.Map_T;
+begin
+
+   loop
+      Put ("Enter key value: ");
+      declare
+         Str         : constant String := Get_Line;
+         Key         : Flags.Key_T;
+         Description : Colors.Color_Set_T;
+         Success     : Boolean;
+      begin
+         exit when Str'Length = 0;
+         Key         := Flags.Key_T'Value (Str);
+         Description := Input.Get;
+         if Flags.Exists (Map, Key) then
+            Flags.Modify (Map, Key, Description, Success);
+         else
+            Flags.Add (Map, Key, Description, Success);
+         end if;
+      end;
+   end loop;
+
+   Put_Line (Flags.Image (Map));
+end Main;
