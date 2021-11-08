2cc480596888f188ef313a76b4c1c976bf28ead2
Léo Germond <germond@adacore.com>
2021-11-08 12:18:40 +0100

    adacut: supports "cut" type

--- contrib/adacut.py
+++ contrib/adacut.py
@@ -9,19 +9,32 @@ class AdaCut:
     RE_DIRECTIVE_PARTIAL = re.compile(r"^\s*\-\-\$.*$")
 
     DIRECTIVES = ["begin", "end", "line"]
-    TYPES = ["answer", "question"]
+    TYPES = ["answer", "question", "cut"]
     TARGETS = ["code", "comment", "all"]
 
     RE_PURE_COMMENT = re.compile(r"^\s*\-\-.*$")
 
-    def __init__(self, mode, default_keeping=True):
-        self.mode = mode
-        self.default_keeping = default_keeping
+    def __init__(self, cut, mode, default_keeping=True):
+        self.cut = cut
+        self.current_cut = 0
+        if self.cut:
+            # Cut mode: throw everything but the given cut
+            self.mode = None
+            self.default_keeping = False
+        else:
+            self.mode = mode
+            self.default_keeping = default_keeping
         self.line = None
         self.block = []
 
+    def match_mode_or_cut(self, typ_name):
+        if self.cut:
+            return self.current_cut == self.cut
+        else:
+            return self.mode == typ_name or self.mode == "keep_all"
+
     def keeping_code_comments_with(self, typ):
-        if typ[0] == self.mode or self.mode == "keep_all":
+        if self.match_mode_or_cut(typ[0]):
             return True, True
         elif typ[1] == "comment":
             return True, False
@@ -76,6 +89,8 @@ class AdaCut:
 
         if directive == "begin":
             self.block.append(typ)
+            if typ[0] == "cut":
+                self.current_cut += 1
         elif directive == "end":
             assert typ == self.block[-1], f"{typ} != {self.block[-1]}"
             self.block = self.block[:-1]
@@ -85,12 +100,17 @@ class AdaCut:
             assert False, "Bug!"
 
 
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("input_file")
     ap.add_argument("-o", "--output-file")
+    ap.add_argument("-c", "--cut", type=int)
+    ap.add_argument("-C", "--cut-counting", action="store_true",
+                    help="Return the number of cuts in the file")
     ap.add_argument(
-        "-m", "--mode", default="question", choices=["answer", "question", "keep_all"]
+        "-m", "--mode", default="question",
+        choices=["answer", "question", "keep_all"]
     )
     args = ap.parse_args()
 
@@ -99,9 +119,12 @@ if __name__ == "__main__":
     else:
         out = sys.stdout
 
-    cut = AdaCut(args.mode)
+    cut = AdaCut(args.cut, args.mode)
     with open(args.input_file) as fin:
         for l in fin:
             lp = cut.new_line(l)
             if lp != None:
                 print(lp, file=out, end="")
+
+    if args.cut_counting:
+        print(cut.current_cut)
---
+++ courses/fundamentals_of_ada/examples/unbounded_controlled_strings/README.md
@@ -0,0 +1,2 @@
+This is an example of unbounded controlled strings, with privacy,
+alongside a main that performs a limited set of operations on them.
