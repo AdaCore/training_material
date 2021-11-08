bff28bf26bb5b09adad840f63fbed97a216c0995
Léo Germond <germond@adacore.com>
2021-11-08 18:55:19 +0100

    240: share delays examples between simple and adv

--- courses/fundamentals_of_ada/examples/delays/src/main.adb
+++ courses/fundamentals_of_ada/examples/delays/src/main.adb
@@ -2,7 +2,8 @@ with Calendar;
 
 procedure Main is
    Relative : Duration := 1.0;
-   Absolute : Calendar.Time := Calendar.Time_Of (2030, 10, 01);
+   Absolute : Calendar.Time
+     := Calendar.Time_Of (2030, 10, 01);
 begin
    delay Relative;
    delay until Absolute;
