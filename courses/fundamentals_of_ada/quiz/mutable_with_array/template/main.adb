a6038c5a6a0c3970be65c51e506c040f2251af62
Léo Germond <germond@adacore.com>
2022-04-29 17:46:29 +0200

    adv 060: add explaination

--- courses/fundamentals_of_ada/quiz/mutable_with_array/template/main.adb
+++ courses/fundamentals_of_ada/quiz/mutable_with_array/template/main.adb
@@ -5,17 +5,24 @@ procedure Main is
       S : String (1 .. Size);
    end record;
    --$ end question
+
    --$ begin cut
-   V1 : R := (6, "Hello");
+   V : R := (6, "Hello");
    --$ end cut
    --$ line cut
-   V1 : R := (5, "Hello");
+   V : R := (5, "Hello");
    --$ begin cut
-   V1 : R (5) := (5, S => "Hello");
+   V : R (5) := (5, S => "Hello");
    --$ end cut
    --$ begin cut
-   V1 : R (6) := (6, S => "Hello");
+   V : R (6) := (6, S => "Hello");
    --$ end cut
+
+   --$ begin answer
+   -- When V is declared without specifying its size, it becomes mutable,
+   -- at this point the :ada:`S'Length = Positive'Last`, causing a Runtime_Error.
+   -- Furthermore the length of "Hello" is 5, it cannot be stored in a String of Length 6.
+   --$ end answer
 begin
    null;
 end Main;
