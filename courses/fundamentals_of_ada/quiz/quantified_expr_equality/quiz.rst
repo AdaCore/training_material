11c6fe10554280af825b13357cfded518cf3f51d
Léo Germond <germond@adacore.com>
2022-05-04 16:06:14 +0200

    adv 080: add quiz

---
+++ courses/fundamentals_of_ada/quiz/quantified_expr_equality/quiz.rst
@@ -0,0 +1,19 @@
+.. code:: Ada
+
+    type T1 is array (1 .. 3) of Integer;
+    type T2 is array (1 .. 3) of Integer;
+
+Which piece(s) of code is(are) correctly performs equality check on A and B?
+
+A. :answermono:`function "=" (A : T1; B : T2) return Boolean is (A = T1 (B))`
+B. | ``function "=" (A : T1; B : T2) return Boolean is``
+   |   ``(for all E1 of A => (for all E2 of B => E1 = E2));``
+C. | ``function "=" (A : T1; B : T2) return Boolean is``
+   |   ``(for some E1 of A => (for some E2 of B => A = B));``
+D. | :answermono:`function "=" (A : T1; B : T2) return Boolean is`
+   |   :answermono:`(for all J in A'Range => A (J) = B (J));`
+
+.. container:: animate
+
+    B. Counterexample: ``A` and ``B`` = ``(0, 1, 0)`` returns :ada:`False`
+    C. Counterexample: ``A`` = (0, 0, 1) and ``B`` = ``(0, 1, 1)`` returns :ada:`True`
---
+++ courses/fundamentals_of_ada/quiz/quantified_expr_equality/template/main.adb
@@ -0,0 +1,35 @@
+-- Which piece(s) of code is(are) correctly performs equality check on A and B?
+procedure Main is
+   --$ begin question
+   type T1 is array (1 .. 3) of Integer;
+   type T2 is array (1 .. 3) of Integer;
+   --$ end question
+
+   --$ line cut
+   function "=" (A : T1; B : T2) return Boolean is (A = T1 (B));
+   --$ begin cut
+   function "=" (A : T1; B : T2) return Boolean is
+     (for all E1 of A => (for all E2 of B => E1 = E2));
+   -- Counterexample: ``A` and ``B`` = ``(0, 1, 0)`` returns :ada:`False`
+   --$ end cut
+   --$ begin cut
+   function "=" (A : T1; B : T2) return Boolean is
+     (for some E1 of A => (for some E2 of B => A = B));
+   -- Counterexample: ``A`` = (0, 0, 1) and ``B`` = ``(0, 1, 1)`` returns :ada:`True`
+   --$ end cut
+   --$ begin cut
+   function "=" (A : T1; B : T2) return Boolean is
+     (for all J in A'Range => A (J) = B (J));
+   --$ end cut
+
+   type Answer_T is record
+      Equal : Boolean;
+      A     : T1;
+      B     : T2;
+   end record;
+
+   Answers : array (Positive range <>) of Answer_T :=
+     ((True, (0, 0, 1), (0, 0, 1)), (False, (0, 0, 1), (0, 1, 1)));
+begin
+   pragma Assert (for all Ans of Answers => Ans.Equal = (Ans.A = Ans.B));
+end Main;
