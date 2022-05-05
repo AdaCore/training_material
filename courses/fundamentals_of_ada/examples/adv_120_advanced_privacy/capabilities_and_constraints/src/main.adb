ec88caae70201cab8d5aac4058760538fbd23c3d
Léo Germond <germond@adacore.com>
2022-05-05 11:48:45 +0200

    adv 120: capabilities and constraints example

---
+++ courses/fundamentals_of_ada/examples/adv_120_advanced_privacy/capabilities_and_constraints/src/main.adb
@@ -0,0 +1,15 @@
+procedure Main is
+   --$ begin cut
+   package P is
+      type T is limited private;
+      -- Does not need to declare any capability
+      -- Declares a constraint: limited
+   private
+      type T is tagged null record;
+      -- Declares a capability: tagged
+      -- Does not need to declare any constraint
+   end P;
+   --$ end cut
+begin
+   null;
+end Main;
