4071813f4e7a4fa95355a36ff135a34b4c7e3b44
Léo Germond <germond@adacore.com>
2022-05-16 17:15:48 +0200

    adv 065: fix quiz

--- courses/fundamentals_of_ada/quiz/primitives_and_classwide/quiz.rst
+++ courses/fundamentals_of_ada/quiz/primitives_and_classwide/quiz.rst
@@ -2,6 +2,7 @@
 
     type T1 is tagged null record;
     type T2 is new T1 with null record;
+    -- Declarations
     V : T2;
 
 Which of the following piece(s) of code allow for calling :ada:`Proc (V)`?
@@ -10,10 +11,11 @@ A. ``procedure Proc (V : T1) is null``
 B. :answermono:`procedure Proc (V : T1'Class) is null`
 C. | ``procedure Proc (V : T1'Class) is null;``
    | ``procedure Proc (V : T2'Class) is null;``
-D. | :answermono:`procedure Proc (V : T1) is null;`
-   | :answermono:`procedure Proc (V : T2) is null;`
+D. :answermono:`procedure Proc (V : T2) is null`
 
 .. container:: animate
 
-    A. Proc is not a primitive
+    A. Not compiling: declared after T1 is frozen
+    B. Correct, but not a primitive
     C. :ada:`T1'Class` contains :ada:`T2'Class`
+    D. Proc is a primitive of T2 **only**
--- courses/fundamentals_of_ada/quiz/primitives_and_classwide/template/main.adb
+++ courses/fundamentals_of_ada/quiz/primitives_and_classwide/template/main.adb
@@ -1,32 +1,39 @@
 -- Which of the following piece(s) of code allow for calling :ada:`Proc (V)`?
 
 procedure Main is
-   --$ begin question
-   type T1 is tagged null record;
-   type T2 is new T1 with null record;
-   V : T2;
-   --$ end question
+   package Pkg is
+      --$ begin question
+      type T1 is tagged null record;
+      type T2 is new T1 with null record;
+      -- Declarations
+      --$ end question
 
-   --$ begin cut
-   procedure Proc (V : T1) is null;
-   --  Proc is not a primitive
-   --$ end cut
+      --$ begin cut
+      procedure Proc (V : T1) is null;
+      --  Not compiling: declared after T1 is frozen
+      --$ end cut
 
-   --$ begin cut
-   procedure Proc (V : T1'Class) is null;
-   --$ end cut
+      --$ begin cut
+      procedure Proc (V : T1'Class) is null;
+      -- Correct, but not a primitive
+      --$ end cut
 
-   --$ begin cut
-   procedure Proc (V : T1'Class) is null;
-   procedure Proc (V : T2'Class) is null;
-   -- :ada:`T1'Class` contains :ada:`T2'Class`
-   --$ end cut
+      --$ begin cut
+      procedure Proc (V : T1'Class) is null;
+      procedure Proc (V : T2'Class) is null;
+      -- :ada:`T1'Class` contains :ada:`T2'Class`
+      --$ end cut
 
-   --$ begin cut
-   procedure Proc (V : T1) is null;
-   procedure Proc (V : T2) is null;
-   --$ end cut
+      --$ begin cut
+      procedure Proc (V : T2) is null;
+      -- Proc is a primitive of T2 **only**
+      --$ end cut
 
+      --$ begin question
+      V : T2;
+      --$ end question
+   end Pkg;
+   use Pkg;
 begin
    Proc (V);
 end Main;
