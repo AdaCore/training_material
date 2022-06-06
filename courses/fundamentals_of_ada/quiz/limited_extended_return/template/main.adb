7db9dcf73dbf86a15f3348cc4d2ebc0d43eb9895
Léo Germond <germond@adacore.com>
2022-06-06 11:11:47 +0200

    limited extended return quiz update

--- courses/fundamentals_of_ada/quiz/limited_extended_return/template/main.adb
+++ courses/fundamentals_of_ada/quiz/limited_extended_return/template/main.adb
@@ -4,26 +4,39 @@ procedure Main is
    type T is limited record
       I : Integer;
    end record;
-   --$ end question
-   
-   --$ line cut
-   function F return T is (I := 1);
-   --$ line cut
-   function F return T is (I => 1);
-   --$ line cut
-   function F return T is (1);
-   --$ begin cut
+
    function F return T is
    begin
+      -- F body...
+   --$ end question
+
+      --$ begin cut
+      return Return : T := (I => 1);
+      -- Using :ada:`return` reserved keyword
+      --$ end cut
+
+      --$ begin cut
+      return Result : T;
+      -- OK, default value
+      --$ end cut
+
+      --$ begin cut
+      return Value := (others => 1);
+      -- Extended return must specify type
+      --$ end cut
+
+      --$ begin cut
       return R : T do
          R.I := 1;
       end return;
+      -- OK
+      --$ end cut
+
+   --$ begin question
    end F;
-   --$ end cut
 
-   --$ line question
    O : T := F;
-
+   --$ end question
 begin
    null;
 end Main;
