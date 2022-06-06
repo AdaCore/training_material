820f14404b2896e8323d7f7fa3b817c2a7f6e0a8
Léo Germond <germond@adacore.com>
2022-06-06 11:54:54 +0200

    incomplete private or not: differenciate quiz

--- courses/fundamentals_of_ada/quiz/incomplete_type/template/main.adb
+++ courses/fundamentals_of_ada/quiz/incomplete_type/template/main.adb
@@ -1,15 +1,22 @@
--- Which of the following types is(are) legal?
+-- In the same scope, which of the following types is(are) legal?
 
 procedure Main is
    --$ line question
    type T;
-   
-   --$ line cut
+
+   --$ begin cut
    type Acc is access T;
-   --$ line cut
+   -- Can :ada:`access` the type
+   --$ end cut
+   --$ begin cut
    type Arr is array (1 .. 10) of T;
-   --$ line cut
+   -- Cannot use the type as a component
+   --$ end cut
+   --$ begin cut
    type T2 is new T;
+   -- Cannot derive from an incomplete type
+   --$ end cut
+   
    --$ begin cut
    type T2 is record
       Acc : access T;
