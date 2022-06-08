131117bfe4d23b9431da94187987902ac0b2cbda
Léo Germond <germond@adacore.com>
2022-06-08 11:58:18 +0200

    Solar system lab : Linux support for VSCode

--- courses/fundamentals_of_ada/labs/solar_system/start_linux_x86-64.sh
+++ courses/fundamentals_of_ada/labs/solar_system/start_linux_x86-64.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+IDE=gnatstudio2
+DIR=$(realpath $(dirname $0))
+
+if ! which gnatstudio2 2>/dev/null >&2; then
+    IDE=code
+fi
+
+which $IDE
 
 INSTALL=$HOME/install
 
-export GPR_PROJECT_PATH=$DIR/game_support:$GPR_PROJECT_PATH
-export GPR_PROJECT_PATH=$DIR/gnat_sdl:$GPR_PROJECT_PATH
+export GPR_PROJECT_PATH="$DIR/game_support:$GPR_PROJECT_PATH"
+export GPR_PROJECT_PATH="$DIR/gnat_sdl:$GPR_PROJECT_PATH"
 
 export HOST=Linux
 
-exec gnatstudio
+exec $IDE
