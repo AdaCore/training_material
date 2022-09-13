ddc2421277d8763156325c006a296913fc21dd53
Leo Germond <germond@adacore.com>
2022-09-13 17:07:44 +0200

    quiz update: black, do not exit on quiz.py fail

--- contrib/quiz_update.py
+++ contrib/quiz_update.py
@@ -18,17 +18,20 @@ quiz = cmd.python.arg(QUIZ_SCRIPT).with_default_kw(out_filter=str)
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    ap.add_argument("-v", "--verbose",
-                    action="store_true")
-    ap.add_argument("-u", "--update",
-                    action="store_true",
-                    help="in case of failure, update the quiz")
+    ap.add_argument("-v", "--verbose", action="store_true")
+    ap.add_argument(
+        "-u",
+        "--update",
+        action="store_true",
+        help="in case of failure, update the quiz",
+    )
     ap.add_argument("--quiz-file-name", default="quiz.rst")
     ap.add_argument("-C", "--no-check", action="store_true")
     ap.add_argument("main_directory", type=Path)
     args = ap.parse_args()
 
     epycs.subprocess.verbose = args.verbose
+    epycs.subprocess.exit_on_error = False
 
     check_failed = False
 
@@ -49,25 +52,28 @@ if __name__ == "__main__":
                 q_old = fread.read()
 
             stderr = None if args.verbose else subprocess.DEVNULL
-            q_new = quiz(inf, stderr=stderr, out_filter=str)
-            
+            q_new = quiz(inf, stderr=stderr, check=True, out_filter=str)
+
             if q_old != q_new:
                 print("\033[31mFAIL\033[0m")
                 check_failed = True
-                diff_lines = list(difflib.unified_diff(
+                diff_lines = list(
+                    difflib.unified_diff(
                         q_old.splitlines(),
                         q_new.splitlines(),
                         fromfile=f"{d.name} (old)",
-                        tofile=f"{d.name} (new)"))
+                        tofile=f"{d.name} (new)",
+                    )
+                )
 
-                pr = lambda s : None
-                color = lambda c : "31" if c == '-' else ("32" if c == "+" else "0")
-                print_colored = lambda s : print(f"\033[{color(s[0])}m{s}\033[0m")
+                pr = lambda s: None
+                color = lambda c: "31" if c == "-" else ("32" if c == "+" else "0")
+                print_colored = lambda s: print(f"\033[{color(s[0])}m{s}\033[0m")
                 for i, l in enumerate(diff_lines):
                     if l:
                         pr(l)
                     if l.startswith("@") or l.startswith("@"):
-                        diff_lines = diff_lines[i+1:]
+                        diff_lines = diff_lines[i + 1 :]
                         pr = print_colored
                 print()
 
@@ -76,8 +82,17 @@ if __name__ == "__main__":
                         print(q_new, file=fwrite, end="")
             else:
                 print("\033[32mOK\033[0m")
+        except subprocess.CalledProcessError as e:
+            print(f"\033[1;41mERROR\033[0m quiz.py {e}")
+            raise
+        except Exception as e:
+            print(f"\033[1;41mERROR\033[0m {e.__class__.__name__} {e}")
+            raise
         except:
-            print("\033[1;41mERROR\033[0m")
+            import traceback
+
+            print(f"\033[1;41mERROR\033[0m unknown error")
+            print(traceback.format_exc())
             raise
 
     if not args.no_check and check_failed:
