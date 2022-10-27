a556df92d70e19cd12c5d8f7c850ffaf7ba4af22
Léo Germond <germond@adacore.com>
2022-10-27 12:38:54 +0200

    remove warnings for elaborated units

--- courses/fundamentals_of_ada/mini_projects/cinema/tests/run_test_display.adb
+++ courses/fundamentals_of_ada/mini_projects/cinema/tests/run_test_display.adb
@@ -1,8 +1,10 @@
 with Test_Suite;
 
+pragma Warnings (Off, "not referenced");
 -- Display
 with Drawable_Chars.Tests;
 with Char_Display_Driver.Tests;
+pragma Warnings (On, "not referenced");
 
 procedure Run_Test_Display is
 begin
