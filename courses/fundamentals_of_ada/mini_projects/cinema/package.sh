11079d180985b394361a9591810c4844738096cf
Léo Germond <germond@adacore.com>
2022-11-08 11:16:50 +0100

    fix packaging

--- courses/fundamentals_of_ada/mini_projects/cinema/package.sh
+++ courses/fundamentals_of_ada/mini_projects/cinema/package.sh
@@ -1,12 +1,14 @@
-set -e
+set -ex
 
 root=$(dirname $(readlink -e $0))
 out=$1
 zip="$out.zip"
 
+test ! -z "$out"
 test -d "$out" || mkdir "$out"
 
-for sub in src answer mini_cinema.gpr mini_cinema.adc resources tests testsuite README.md; do
+for sub in src answer mini_cinema.gpr mini_cinema.adc include logs resources tests testsuite README.md; do
+    test -f "$root/$sub" -o -d "$root/$sub"
     cp -r "$root/$sub" "$out"
 done
 
