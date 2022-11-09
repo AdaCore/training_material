59515cfe9a0530795f84d80d3abd88712d7dba63
Léo Germond <germond@adacore.com>
2022-11-09 12:59:09 +0100

    package mini-cinema with custom script

--- courses/fundamentals_of_ada/mini_projects/cinema/package.sh
+++ courses/fundamentals_of_ada/mini_projects/cinema/package.sh
@@ -1,19 +1,21 @@
 set -ex
 
 root=$(dirname $(readlink -e $0))
-out=$1
-zip="$out.zip"
+out="$root/pkg"
+zip="$1"
+test ! -z "$zip"
 
-test ! -z "$out"
 test -d "$out" || mkdir "$out"
+rm -rf "$out/*"
 
 for sub in src answer mini_cinema.gpr mini_cinema.adc include logs resources tests testsuite README.md; do
     test -f "$root/$sub" -o -d "$root/$sub"
     cp -r "$root/$sub" "$out"
 done
 
+(
 cd $(dirname $out)
 
-rm -f "$zip"
+rm -f "$zip" 2>/dev/null
 zip -r "$zip" "$out"
-
+)
