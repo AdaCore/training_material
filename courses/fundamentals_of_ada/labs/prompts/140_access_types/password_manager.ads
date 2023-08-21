8dd988e5f65141fb83538d94b03fd36a56b1a349
Michael Frank <frank@adacore.com>
2023-08-21 18:39:55 +0000

    Simplify the Access Types lab

---
+++ courses/fundamentals_of_ada/labs/prompts/140_access_types/password_manager.ads
@@ -0,0 +1,15 @@
+package Password_Manager is
+
+   type Login_T is (Email, Banking, Amazon, Streaming);
+   type Password_T is null record;
+   --  Need fields for count and password string
+
+   --  These aren't right, but they compile!
+   function Update
+     (Login : Login_T)
+      return Password_T;
+   function View
+     (Login : Login_T)
+      return Password_T;
+
+end Password_Manager;
