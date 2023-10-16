2b2ca443cd3f29a556fcef03ff02343555c832e7
Leo Germond <germond@adacore.com>
2023-10-16 17:20:25 +0200

    lint shell script

--- contrib/ci/package_labs.sh
+++ contrib/ci/package_labs.sh
@@ -2,11 +2,11 @@
 set -e
 
 orig=$1
-OUT=$(realpath $2)
-ar=$OUT/$(basename $orig).zip
-if [ ! -f $orig/package.sh ]; then
-    (cd $orig && git archive HEAD -o $ar)
+OUT=$(realpath "$2")
+ar=$OUT/$(basename "$orig").zip
+if [ ! -f "$orig/package.sh" ]; then
+    (cd "$orig" && git archive HEAD -o "$ar")
 else
-    (cd $orig && bash package.sh $ar)
+    (cd "$orig" && bash package.sh "$ar")
 fi
-(cd $OUT && zip $ar *.pdf || true)
+(cd "$OUT" && zip "$ar" *.pdf || true)
