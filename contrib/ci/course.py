5765516233bf8bae5bf17bc2c7ea174aeac3eef2
Leo Germond <germond@adacore.com>
2024-03-12 14:47:55 +0100

    can specify DO_NOT_MERGE.SINGLE_COURSE on CI
    + checks on GitLab's CI that it is not specified on MR or main

--- contrib/ci/course.py
+++ contrib/ci/course.py
@@ -4,6 +4,9 @@ import argparse
 from pathlib import Path
 
 
+SINGLE_COURSE_FILE = Path(__file__).resolve().parents[2] / "DO_NOT_MERGE.SINGLE_COURSE"
+
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("action", choices=["find"])
@@ -16,9 +19,12 @@ if __name__ == "__main__":
     if args.action == "find":
         # courses are all the direct subdirs that contain a course.toml file,
         # and any txt files in those dirs is a course variant
-        courses = [d.parent for d in args.dir.glob("**/course.toml")]
-
-        courses += [f for d in courses for f in d.glob("*.txt")]
+        if SINGLE_COURSE_FILE.is_file():
+            with open(SINGLE_COURSE_FILE) as f:
+                courses = [args.dir / f.read().strip()]
+        else:
+            courses = [d.parent for d in args.dir.glob("**/course.toml")]
+            courses += [f for d in courses for f in d.glob("*.txt")]
 
         courses_str = [str(c.relative_to(args.dir)) for c in courses]
 
