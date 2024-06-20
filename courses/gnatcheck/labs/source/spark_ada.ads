d742d639f7147d9dab4d39c72a7f8f9d2e2c4d25
Michael Frank <frank@adacore.com>
2024-06-20 17:17:18 +0000

    Resolve "Rename CodePeer to GNAT SAS"

--- courses/gnatcheck/labs/source/spark_ada.ads
+++
@@ -1,7 +0,0 @@
-package Spark_Ada is
-
-   One   : Boolean;
-   Two   : Boolean;
-   Three : Boolean := One < Two;
-
-end Spark_Ada;
--- courses/gnattest/labs/source/src/loading_dock.ads
+++
@@ -1,7 +0,0 @@
-package Loading_Dock is
-
-   -- Manifest is the name of a file.
-   -- File format is "item name | count"
-   procedure Load_From_Manifest (Manifest : String);
-
-end Loading_Dock;
