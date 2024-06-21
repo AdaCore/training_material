4d3ae1fe04e04bb2c931631982af2f513f8ed855
Leo Germond <germond@adacore.com>
2024-06-21 16:46:33 +0200

    mini cinema: fix template & questions

--- courses/fundamentals_of_ada/mini_projects/cinema/template/sprites/pixels.ads
+++ courses/fundamentals_of_ada/mini_projects/cinema/template/sprites/pixels.ads
@@ -11,7 +11,7 @@ package Pixels is
    
    type Pixel_T is record
       --$ begin question
-      -- TODO: At least R, G, B pixel components 
+      -- TODO: R, G, B, A pixel components
       --      replace those dummy variables with the proper name and type
       DUMMY1, DUMMY2, DUMMY3, DUMMY4 : Integer;
       --$ end question
