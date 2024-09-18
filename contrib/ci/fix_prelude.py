606c1094d370b2a027417791d1046a2749b04877
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2024-09-18 11:07:31 -0400

    formatting issues

--- contrib/ci/fix_prelude.py
+++ contrib/ci/fix_prelude.py
@@ -26,55 +26,59 @@ def files_digest(files):
             h.update(frd.read())
     return h.digest()
 
-ROLES = (".. role:: ada(code)\n" +
-         "    :language: Ada\n" +
-         "\n" +
-         ".. role:: C(code)\n" +
-         "    :language: C\n" +
-         "\n" +
-         ".. role:: cpp(code)\n" +
-         "    :language: C++\n" +
-         "\n" +
-         "..")
+ROLES = (
+    ".. role:: ada(code)\n"
+    + "    :language: Ada\n"
+    + "\n"
+    + ".. role:: C(code)\n"
+    + "    :language: C\n"
+    + "\n"
+    + ".. role:: cpp(code)\n"
+    + "    :language: C++\n"
+    + "\n"
+    + "..")
 
-SYMBOLS = (".. |rightarrow| replace:: :math:`\\rightarrow`\n" +
-           ".. |forall| replace:: :math:`\\forall`\n" +
-           ".. |exists| replace:: :math:`\\exists`\n" +
-           ".. |equivalent| replace:: :math:`\\iff`\n" +
-           ".. |le| replace:: :math:`\\le`\n" +
-           ".. |ge| replace:: :math:`\\ge`\n" +
-           ".. |lt| replace:: :math:`<`\n" +
-           ".. |gt| replace:: :math:`>`\n" +
-           ".. |checkmark| replace:: :math:`\\checkmark`\n" +
-           "\n" +
-           "..")
+SYMBOLS = (
+    ".. |rightarrow| replace:: :math:`\\rightarrow`\n"
+    + ".. |forall| replace:: :math:`\\forall`\n"
+    + ".. |exists| replace:: :math:`\\exists`\n"
+    + ".. |equivalent| replace:: :math:`\\iff`\n"
+    + ".. |le| replace:: :math:`\\le`\n"
+    + ".. |ge| replace:: :math:`\\ge`\n"
+    + ".. |lt| replace:: :math:`<`\n"
+    + ".. |gt| replace:: :math:`>`\n"
+    + ".. |checkmark| replace:: :math:`\\checkmark`\n"
+    + "\n"
+    + "..")
 
 def validate_roles(content):
     return content == ROLES
-    
+
+
 def validate_symbols(content):
     return content == SYMBOLS
 
+
 def compare_content(title, actual_str, expected_str):
     retval = []
-    actual = actual_str.split('\n')
-    expected = expected_str.split('\n')
+    actual = actual_str.split("\n")
+    expected = expected_str.split("\n")
     l_actual = len(actual)
     l_expected = len(expected)
-    length = min ([l_actual, l_expected])
+    length = min([l_actual, l_expected])
     for idx in range(0, length):
         if actual[idx] != expected[idx]:
             retval.append("In " + title)
-            retval.append("  Line " + str(idx+1))
+            retval.append("  Line " + str(idx + 1))
             retval.append("    Actual  : " + actual[idx])
             retval.append("    Expected: " + expected[idx])
     if len(retval) == 0 and l_actual < l_expected:
         retval.append("In " + title + " missing:")
-        for idx in range (l_actual, l_expected):
+        for idx in range(l_actual, l_expected):
             retval.append("    " + expected[idx])
     elif len(retval) == 0 and l_actual > l_expected:
         retval.append("In " + title + " extra:")
-        for idx in range (l_expected, l_actual):
+        for idx in range(l_expected, l_actual):
             retval.append("    " + actual[idx])
     return retval
 
@@ -84,12 +88,12 @@ def process_one_file(filename, interactive):
         failures = []
     else:
         failures = ""
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         content = file.read()
-        pieces = content.split('PRELUDE: ')
+        pieces = content.split("PRELUDE: ")
         for section in pieces:
             stripped = section.strip()
-            name, content = section.split('\n', 1)
+            name, content = section.split("\n", 1)
             content = content.strip()
             if name == "ROLES":
                 if not validate_roles(content):
@@ -104,14 +108,12 @@ def process_one_file(filename, interactive):
                     else:
                         failures = failures + " Roles"
     return failures
-            
+
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument(
-        "--files-to-check",
-        type=Path,
-        default=CONTRIB / "rst_files_with_prelude.txt"
+        "--files-to-check", type=Path, default=CONTRIB / "rst_files_with_prelude.txt"
     )
     ap.add_argument("--interactive", action="store_true")
     args = ap.parse_args()
@@ -133,7 +135,7 @@ if __name__ == "__main__":
                 if args.interactive:
                     print("FAIL: " + str(one))
                     for line in failures:
-                        print ("  " + line)
+                        print("  " + line)
                 else:
                     print("FAIL: " + str(one) + failures)
 
