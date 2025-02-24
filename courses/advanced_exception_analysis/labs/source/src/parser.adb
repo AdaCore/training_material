d2ab24c169ceee858d1ad2e99261eea5774992b9
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2025-02-24 16:20:21 -0500

    Change "field" and "element" to "component"
    Except where "Element" matches docuementation

--- courses/advanced_exception_analysis/labs/source/src/parser.adb
+++ courses/advanced_exception_analysis/labs/source/src/parser.adb
@@ -5,7 +5,7 @@ with Strings;     use Strings;
 
 package body Parser is
 
-   type Element_T is record
+   type Component_T is record
       Line_Number : Integer;
       Category    : Category_T;
       Description : String_T;
@@ -13,7 +13,7 @@ package body Parser is
       Cost        : Cost_T;
    end record;
 
-   Database       : array (1 .. 10) of Element_T;
+   Database       : array (1 .. 10) of Component_T;
    Database_Count : Integer := 0;
 
    procedure Load (Filename : String) is
@@ -23,29 +23,29 @@ package body Parser is
       while not End_Of_File (File)
       loop
          declare
-            Pieces  : Strings_T := Split (Get_Line (File));
-            Element : Element_T;
+            Pieces    : Strings_T := Split (Get_Line (File));
+            Component : Component_T;
          begin
-            Element.Line_Number       := Integer (Line (File) - 1);
-            Element.Category          := Convert (Pieces (1).all);
-            Element.Description       := Pieces (2);
-            Element.Quantity          := Convert (Pieces (3).all);
-            Element.Cost              := Convert (Pieces (4).all);
-            Database_Count            := Database_Count + 1;
-            Database (Database_Count) := Element;
+            Component.Line_Number       := Integer (Line (File) - 1);
+            Component.Category          := Convert (Pieces (1).all);
+            Component.Description       := Pieces (2);
+            Component.Quantity          := Convert (Pieces (3).all);
+            Component.Cost              := Convert (Pieces (4).all);
+            Database_Count              := Database_Count + 1;
+            Database (Database_Count)   := Component;
          end;
       end loop;
    end Load;
 
    procedure Print is
    begin
-      for Element of Database (1 .. Database_Count)
+      for Component of Database (1 .. Database_Count)
       loop
-         Put (Element.Line_Number'Image & ": ");
-         Put (Element.Description.all & " (");
-         Put (Convert (Element.Category) & ") ");
-         Put (Convert (Element.Quantity) & " at $");
-         Put_Line (Convert (Element.Cost));
+         Put (Component.Line_Number'Image & ": ");
+         Put (Component.Description.all & " (");
+         Put (Convert (Component.Category) & ") ");
+         Put (Convert (Component.Quantity) & " at $");
+         Put_Line (Convert (Component.Cost));
       end loop;
    end Print;
 
