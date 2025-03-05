19cc9d7103569bb55b6468fa35b982006a2d384f
Leo Germond <germond@adacore.com>
2025-03-05 17:21:39 +0100

    removed example_extract and the unused containers examples

--- courses/fundamentals_of_ada/examples/containers/extracts/decl_instances.adb
+++
@@ -1,7 +0,0 @@
-Student_Per_Day : Pkg_Vectors.Vector (5);
--- Warning: initial size is 0, using an Empty_Vector as
---          initial value would mean a *capacity* of 0!
-
-Received_Parcels : Pkg_Sets.Set := Pkg_Sets.Empty_Set;
-
-Math_Constants : Pkg_Maps.Map := Pkg_Maps.Empty_Map;
