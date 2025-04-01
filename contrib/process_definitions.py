438fd92bf78624fba80bedc0aecb0c1a372c631b
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2025-04-01 13:16:08 -0400

    Update script to illustrate full process for definitions
    Modification of files is TBD
    Debug subprograms added to show content
    Lots more comments

--- contrib/process_definitions.py
+++ contrib/process_definitions.py
@@ -1,7 +1,65 @@
+"""
+This script is looking for the definition role (:dfn:) in
+RST files, so that it can add these definitions to the
+list of things that this module provides (and therefore
+will appear in the "PROVIDES" section of the module prefix).
+
+This script assumes that the modules are "chapterized" -
+meaning that the module only has the module prefix and
+"include" directives - no definitions should be in this
+file.  It will parse the "include" directives in 
+the module file looking for definitions in the chapter
+files - but this is not recursive (it will not search
+for "include" directives in the chapter files).
+(Note that "include" directives should be using "/"
+as a path separator).
+"""
+
 import argparse
 import os
 
-GLOBAL_filenames = []
+######################
+## DEBUG PROCESSING ##
+######################
+
+GLOBAL_all_definitions = []
+
+
+def debug_all_definitions(chapter, definitions):
+
+    global GLOBAL_all_definitions
+
+    for one in definitions:
+        GLOBAL_all_definitions.append(one + " => " + chapter)
+
+
+def debug_print_by_chapter(chapter_definitions):
+    print("** Chapter Definitions **")
+    for chapter in sorted(chapter_definitions.keys()):
+        if len(chapter_definitions[chapter]) > 0:
+            print("   * " + chapter + " *")
+            for one in sorted(chapter_definitions[chapter]):
+                print("      " + one)
+
+
+def debug_print_by_module(module_name, definitions):
+    if len(definitions) > 0:
+        print("   * " + module_name + " *")
+        for one in sorted(definitions):
+            print("      " + one)
+
+
+def debug_print_all_definitions():
+    global GLOBAL_all_definitions
+
+    print("** All Definitions **")
+    for one in sorted(GLOBAL_all_definitions):
+        print("   " + one)
+
+
+##########################
+## END DEBUG PROCESSING ##
+##########################
 
 
 def is_rst(filename):
@@ -10,21 +68,6 @@ def is_rst(filename):
     """
 
     return os.path.splitext(filename)[1].lower() == ".rst"
-
-
-def add_filename_to_list(filename):
-    """
-    Make sure "filename" is in a platform-dependent state, then
-    add it to the list of files we care about
-    """
-
-    global GLOBAL_filenames
-
-    partial = filename.replace("\\", "/")
-    pieces = filename.split("/")
-    to_append = os.path.join(*pieces)
-    if to_append not in GLOBAL_filenames:
-        GLOBAL_filenames.append(to_append)
 
 
 def find_definitions(filename):
@@ -52,47 +95,55 @@ def find_definitions(filename):
     return retval
 
 
-def find_included_files(filename):
+def find_chapter_files(module):
     """
-    Search 'filename' for an inclusion of an RST file.
-    If found, add it to the list of files we care about
+    Return a list of files in "include" directives for the module
     """
 
-    with open(filename, "r") as file:
+    chapter_files = []
+    with open(module, "r") as file:
         for line in file:
             if line.startswith(".. include::"):
                 included = line[13 : len(line)].strip()
                 if is_rst(included):
-                    add_filename_to_list(included)
+                    chapter_files.append(included)
+
+    return chapter_files
 
 
-def build_filenames_list(filename):
+def find_all_definitions(chapter_files):
     """
-    Add 'filename' and all the files it includes to the
-    list of files we care about
-    """
-
-    if filename not in GLOBAL_filenames:
-        add_filename_to_list(filename)
-        find_included_files(filename)
-
-
-def find_all_definitions():
-    """
-    For each file we care about, print out the
-    definitions we find.
+    Build a dictionary where the key is each element in chapter_files
+    and the content is the list of definitions in that file
     """
 
-    global GLOBAL_filenames
+    all_definitions = {}
+    for chapter in sorted(chapter_files):
+        all_definitions[chapter] = find_definitions(chapter)
+        debug_all_definitions(chapter, all_definitions[chapter])
 
-    output = []
-    for filename in sorted(GLOBAL_filenames):
-        definitions = find_definitions(filename)
-        for one in definitions:
-            output.append(one + " => " + filename)
+    return all_definitions
 
-    for d in sorted(output):
-        print(d)
+
+def update_one_module(module_name, chapters, chapter_definitions):
+    """
+    Search for definitions found in any of the chapters for this
+    module. If found, update the "PROVIDES" section of the module
+    prefix with the definitions.
+    """
+
+    definitions = []
+    for chapter in chapters:
+        for definition in chapter_definitions[chapter]:
+            if not (definition in definitions):
+                definitions.append(definition)
+    definitions = sorted(definitions)
+
+    """
+    TBD - actual updating of the module file
+    """
+
+    debug_print_by_module(module_name, definitions)
 
 
 if __name__ == "__main__":
@@ -107,11 +158,29 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    module_files = {}
+    chapter_files = []
+
     if not os.path.isdir(args.directory):
         print(args.directory + " does not exist")
     else:
         for file in os.listdir(args.directory):
             if is_rst(file):
-                build_filenames_list(os.path.join(args.directory, file))
+                module = os.path.join(args.directory, file)
+                module_files[module] = find_chapter_files(module)
+                for file in module_files[module]:
+                    if not (file in chapter_files):
+                        chapter_files.append(file)
 
-    find_all_definitions()
+    chapter_definitions = find_all_definitions(chapter_files)
+
+    ### DEBUG ###
+    debug_print_by_chapter(chapter_definitions)
+
+    debug_print_all_definitions()
+
+    print("** All Modules **")
+    ### END DEBUG ###
+
+    for module in module_files.keys():
+        update_one_module(module, module_files[module], chapter_definitions)
