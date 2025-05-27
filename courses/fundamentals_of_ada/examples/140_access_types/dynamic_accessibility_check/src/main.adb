8cc819745b8712cb0374df254c44688cf0ea4c2f
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2025-05-27 11:32:16 -0400

    Show the problem with invalid dynamic access

--- courses/fundamentals_of_ada/examples/140_access_types/dynamic_accessibility_check/src/main.adb
+++ courses/fundamentals_of_ada/examples/140_access_types/dynamic_accessibility_check/src/main.adb
@@ -1,15 +1,20 @@
+with Ada.Text_IO; use Ada.Text_IO;
 procedure Main is
+--start_snippet
    type Acc is access all Integer;
    O : Acc;
-
+   Outer : aliased Integer := 123;
    procedure Set_Value (V : access Integer) is
    begin
+      Put_Line (V.all'Image);
       O := Acc (V);
    end Set_Value;
 begin
+   Set_Value (Outer'Access);
    declare
-      O2 : aliased Integer := 2;
+      Inner : aliased Integer := 987;
    begin
-      Set_Value (O2'Access);
+      Set_Value (Inner'Access);
    end;
+--end_snippet
 end Main;
