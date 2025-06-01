890639cc698af98b766466e28bf0fa399fe3d4ba
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2025-06-01 13:58:10 -0400

    Add shell to shell script

--- courses/spark_for_ada_programmers/labs/source/gpr_project_path.sh
+++ courses/spark_for_ada_programmers/labs/source/gpr_project_path.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/env bash
+
 echo Find your SPARKPRO installation folder.
 echo Set your GPR_PROJECT_PATH to include the "lib/gnat" subfolder
 echo Your command should like like \"export GPR_PROJECT_PATH=\<SPARK install\>/lib/gnat:\$PATH\"
