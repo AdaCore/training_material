9b6658db46220383c612a6b25ae87bf4c64680f9
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2025-06-01 12:21:58 -0400

    Add scripts to (help) set GPR_PROJECT_PATH
    Install dir for Linux is not consistent, so the script is just
    some messages

---
+++ courses/spark_for_ada_programmers/labs/source/gpr_project_path.sh
@@ -0,0 +1,3 @@
+echo Find your SPARKPRO installation folder.
+echo Set your GPR_PROJECT_PATH to include the "lib/gnat" subfolder
+echo Your command should like like \"export GPR_PROJECT_PATH=\<SPARK install\>/lib/gnat:\$PATH\"
