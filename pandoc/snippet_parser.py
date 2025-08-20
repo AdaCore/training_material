ba64dbc26016949ec235a71ed272a290d01b5965
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2025-08-20 16:04:51 -0400

    Allow "source_include" container to be more flexible
    No longer need "--" or "--|" in front of search strings

--- pandoc/snippet_parser.py
+++ pandoc/snippet_parser.py
@@ -1,5 +1,15 @@
 import os
 
+def line_matches (line, search_str):
+    if len(search_str) > 0:
+        stripped = line.strip()
+        if stripped == search_str:
+            return True
+        elif stripped == "--" + search_str:
+            return True
+        elif stripped == "--|" + search_str:
+            return True
+    return False
 
 def source_file_contents(filename, start_after, end_before):
     retval = ""
@@ -16,10 +26,10 @@ def source_file_contents(filename, start_after, end_before):
             for line in the_file:
                 # if we're not echoing, then look for the starting text
                 if not echo_on:
-                    if len(start_after) > 0 and start_after == line.strip():
+                    if line_matches (line, start_after):
                         echo_on = True
                 # if we are echoing and we find the ending text, we're done
-                elif len(end_before) > 0 and end_before == line.strip():
+                elif line_matches (line, end_before):
                     break
                 # Do not echo lines with tags we're not looking for
                 elif line.strip().startswith("--|"):
