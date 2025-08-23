6ac865fc63505329fe0761f92d5f8783ac756d65
Michael Frank <frank@adacore.com>
2025-08-23 02:40:12 +0000

    Resolve "Extract the "Hello World" Setup from the Overview Module"

---
+++ courses/ada_essentials/013_hello_world/lab/hello_world/prompt/hello_world.adb
@@ -0,0 +1,7 @@
+--  Everyone's first program
+with Ada.Text_IO;
+procedure Hello_World is
+begin
+   --  Replace '<something>' with 'Hello, World'
+   Ada.Text_IO.Put_Line ("<something>");
+end Hello_World;
