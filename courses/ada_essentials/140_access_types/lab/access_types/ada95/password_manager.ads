f45821465181a7d6fed8f27a5bcee5cc68e0526c
Michael Frank <frank@adacore.com>
2025-10-30 13:12:32 +0000

    Resolve "Revamp Access Types module"

--- courses/ada_essentials/140_access_types/lab/access_types/ada95/password_manager.ads
+++ courses/ada_essentials/140_access_types/lab/access_types/ada95/password_manager.ads
@@ -1,20 +1,19 @@
 package Password_Manager is
 
    type Login_T is (Email, Banking, Amazon, Streaming);
-   type Password_String_T is access String;
+   type String_Access_T is access String;
    type Password_T is record
       Count    : Natural;
-      Password : Password_String_T;
+      Password : String_Access_T;
    end record;
 
-   type Modifiable_T is access all Password_T;
-   type Viewable_T is access constant Password_T;
+   type Password_Access_T is access Password_T;
 
    function Update
      (Login : Login_T)
-      return Modifiable_T;
+      return Password_Access_T;
    function View
      (Login : Login_T)
-      return Viewable_T;
+      return Password_T;
 
 end Password_Manager;
