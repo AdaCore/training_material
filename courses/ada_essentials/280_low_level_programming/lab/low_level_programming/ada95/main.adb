1cef961a3965a066e54f2eb4a263e37571ffc575
Michael Frank <frank@adacore.com>
2025-11-04 15:35:59 +0000

    Resolve "Update Low Level programming lab to remove console input"

--- courses/ada_essentials/280_low_level_programming/lab/low_level_programming/ada95/main.adb
+++ courses/ada_essentials/280_low_level_programming/lab/low_level_programming/ada95/main.adb
@@ -1,84 +1,30 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Messages;
-procedure Main is
-   Message : Messages.Message_T;
-   function Get_Line return String is
-      S : String (1 .. 100);
-      L : Integer;
-   begin
-      Get_Line (S, L);
-      return S (1 .. L);
-   end Get_Line;
-   function Command return Messages.Command_T is
-   begin
-      loop
-         Put ("Command (");
-         for E in Messages.Command_T
-         loop
-            Put (Messages.Command_T'Image (E) & " ");
-         end loop;
-         Put ("): ");
-         begin
-            return Messages.Command_T'Value (Get_Line);
-         exception
-            when others =>
-               Put_Line ("Illegal");
-         end;
-      end loop;
-   end Command;
-   function Value return Positive is
-   begin
-      loop
-         Put ("Value: ");
-         begin
-            return Positive'Value (Get_Line);
-         exception
-            when others =>
-               Put_Line ("Illegal");
-         end;
-      end loop;
-   end Value;
-   function Text return String is
-   begin
-      Put ("Text: ");
-      return Get_Line;
-   end Text;
 
-   procedure Create is
-      C : constant Messages.Command_T := Command;
-      V : constant Positive           := Value;
-      T : constant String             := Text;
-   begin
-      Message := Messages.Create
-          (Command => C,
-           Value   => V,
-           Text    => T);
-   end Create;
+procedure Main is
+
+   Outgoing_Message : Messages.Message_T;
+
    procedure Read is
-      Valid : Boolean;
+      Incoming_Message : Messages.Message_T;
+      Is_Valid         : Boolean;
    begin
-      Messages.Read (Message, Valid);
-      Ada.Text_IO.Put_Line("Message valid: " & Boolean'Image (Valid));
+      Messages.Read (Incoming_Message, Is_Valid);
+      if Is_Valid then
+         Put_Line ("Read successful");
+      else
+         Messages.Print ("Read failure - Message received", Incoming_Message);
+      end if;
    end Read;
+
 begin
-   loop
-      Put ("Create Write Read Print: ");
-      declare
-         Command : constant String := Get_Line;
-      begin
-         exit when Command'Length = 0;
-         case Command (Command'First) is
-            when 'c' | 'C' =>
-               Create;
-            when 'w' | 'W' =>
-               Messages.Write (Message);
-            when 'r' | 'R' =>
-               Read;
-            when 'p' | 'P' =>
-               Messages.Print (Message);
-            when others =>
-               null;
-         end case;
-      end;
-   end loop;
+
+   Outgoing_Message :=
+     Messages.Create
+       (Kind => Messages.Speed, Value => 123.456, Text => "Hello!!");
+   Messages.Print ("Sending message", Outgoing_Message);
+   Messages.Write (Outgoing_Message);
+   Read;
+   delay 5.0;
+   Read;
 end Main;
