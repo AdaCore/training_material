4ee7e18ea256e80b595cf4aba29caba40e585bbc
Julien Berkane~ <berkane@adacore.com>
2026-04-15 13:37:53 +0200

    [!] First push

---
+++ courses/rust_essentials/030_types_and_values/lab/prompt/target/debug/deps/prompt.d
@@ -0,0 +1,5 @@
+C:\Users\berkane\workspace\Issues\Issue472\material\courses\rust_essentials\030_types_and_values\lab\prompt\target\debug\deps\prompt.d: src\main.rs
+
+C:\Users\berkane\workspace\Issues\Issue472\material\courses\rust_essentials\030_types_and_values\lab\prompt\target\debug\deps\prompt.exe: src\main.rs
+
+src\main.rs:
