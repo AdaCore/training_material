a615e1dccc070dc1df5e963c7d71ec95c8e9d4fb
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2026-04-16 10:34:51 -0400

    Update lab text, pretty-print examples

--- courses/rust_essentials/060_references/88-references.lab.rst
+++ courses/rust_essentials/060_references/88-references.lab.rst
@@ -1,46 +1,15 @@
-====================
-Exercise: Geometry
-====================
+=====
+Lab
+=====
 
 ------------------
-Geometry Problem
+Lab Instructions
 ------------------
 
-We will create a few utility functions for 3-dimensional geometry,
-representing a point as :rust:`[f64;3]`. It is up to you to determine the
-function signatures.
+* Solve for compilation errors
+* Follow the hints!
+* Success is
 
-.. code:: rust
+  * Code that compiles
+  * ... and that follows any behavior indicated within the hints
 
-   // Calculate the magnitude of a vector by summing the squares of its coordinates
-   // and taking the square root. Use the `sqrt()` method to calculate the square
-   // root, like `v.sqrt()`.
-
-   fn magnitude(...) -> f64 {
-       todo!()
-   }
-
-   // Normalize a vector by calculating its magnitude and dividing all of its
-   // coordinates by that magnitude.
-
-   fn normalize(...) {
-       todo!()
-   }
-
-Use the following :rust:`main` to test your work.
-
-.. container:: source_include 060_references/src/060_references.rs :start-after://ANCHOR-main :code:rust
-
--------------------
-Geometry Solution
--------------------
-
-.. container:: source_include 060_references/src/060_references.rs :start-after://ANCHOR-solution :end-before://ANCHOR-main :code:rust
-
-------------------------
-Additional Information
-------------------------
-
-Note that in :rust:`normalize` we wrote :rust:`*item /= mag` to modify each element.
-
-This is because we’re iterating using a mutable reference to an array, which causes the :rust:`for` loop to give mutable references to each element.
--- courses/rust_essentials/060_references/lab/prompt/src/main.rs
+++ courses/rust_essentials/060_references/lab/prompt/src/main.rs
@@ -14,7 +14,10 @@ fn main() {
     // TASK 1 - Get the "println" to print double the field contents
     // Hint: Cannot modify a shared reference
     println!("Task 1 - Observer");
-    let angle = ReferencesStruct { count: 12, value: 34.5, };
+    let angle = ReferencesStruct {
+        count: 12,
+        value: 34.5,
+    };
     let ref_angle = &angle;
     *ref_angle.count = *ref_angle.count * 2;
     *ref_angle.value = *ref_angle.value * 2.0;
@@ -24,8 +27,14 @@ fn main() {
     // TASK 2 - Use one reference object to print the contents of both objects
     // Hint: A shared reference can be mutable!
     println!("Task 2 - Rebinder");
-    let bricks = ReferencesStruct { count: 6, value: 78.9, };
-    let buildings = ReferencesStruct { count: 1, value: 2.3, };
+    let bricks = ReferencesStruct {
+        count: 6,
+        value: 78.9,
+    };
+    let buildings = ReferencesStruct {
+        count: 1,
+        value: 2.3,
+    };
     let ref_both = &bricks;
     println!("{} {}", *ref_both->count, *ref_both->value);
     ref_both = &buildings;
@@ -35,7 +44,10 @@ fn main() {
     // TASK 3 - Divide the pointed-to data contents by 2
     // Hint: Need a mutable reference
     println!("Task 3 - Modifier");
-    let cars = ReferencesStruct { count: 67, value: 8.9, };
+    let mut cars = ReferencesStruct {
+        count: 67,
+        value: 8.9,
+    };
     let ref_cars = &mut cars;
     let mut ref_cars = &cars;
     ref_cars->count = ref_cars->count / 2;
@@ -46,23 +58,41 @@ fn main() {
     // TASK 4 - Print the two objects and clear the contents all using one reference
     // Hint: The mutable reference needs to be mutable
     println!("Task 4 - Free Agent");
-    let dog = ReferencesStruct { count: 1, value: 2.3, };
-    let duck = ReferencesStruct { count: 4, value: 5.6, };
+    let mut dog = ReferencesStruct {
+        count: 1,
+        value: 2.3,
+    };
+    let mut duck = ReferencesStruct {
+        count: 4,
+        value: 5.6,
+    };
     let ref_both = &dog;
     println!("{} {}", ref_both.count, ref_both.value);
-    ref_both = ReferencesStruct {count: 0, value: 0.0, };
+    ref_both = ReferencesStruct {
+        count: 0,
+        value: 0.0,
+    };
     println!("{} {}", ref_both.count, ref_both.value);
     ref_both = &duck;
     println!("{} {}", ref_both.count, ref_both.value);
-    ref_both = ReferencesStruct {count: 0, value: 0.0, };
+    ref_both = ReferencesStruct {
+        count: 0,
+        value: 0.0,
+    };
     println!("{} {}", ref_both.count, ref_both.value);
 
 
     // TASK 5 - Fix the dangling reference
     // Hint: Need to make the referenced object last longer
     let ref_emu = {
-        let first = ReferencesStruct {count: 9, value: 8.7};
-        let second = ReferencesStruct {count: first.count + 1, value: first.value - 1.1};
+        let first = ReferencesStruct {
+            count: 9,
+            value: 8.7,
+        };
+        let second = ReferencesStruct {
+            count: first.count + 1,
+            value: first.value - 1.1,
+        };
         &second
     };
     println!("{} {}", ref_emu.count, ref_emu.value);
@@ -70,7 +100,7 @@ fn main() {
     // TASK 6 - Use a single reference to print all possible slices of 'floats'
     // Hint: Reference refers to object, not length
     // Note: "{:?}" is used to print the contents of the array
-    let floats: [f64;5] = [1.1, 22.22, 333.333, 4444.4444, 55555.555555];
+    let floats: [f64; 5] = [1.1, 22.22, 333.333, 4444.4444, 55555.555555];
     let ref_floats: &[f64] = &floats[0..1];
     println!("{:?}", ref_floats);
     ref_floats[1..3] = &floats[1..3];
