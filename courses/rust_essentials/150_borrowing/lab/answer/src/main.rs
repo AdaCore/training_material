5b7b0ddb711216eb8595ad046ab013a75d28ca30
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2026-04-20 15:57:35 -0400

    Clean up Borrow Lab to make it easier

--- courses/rust_essentials/150_borrowing/lab/answer/src/main.rs
+++ courses/rust_essentials/150_borrowing/lab/answer/src/main.rs
@@ -4,136 +4,196 @@
 //! Fix all the compile errors below by following the hints provided
 //!
 
-use std::cell::Cell;
-use std::cell::RefCell;
+fn task_1() {
+    println!("Task 1 - Mutable and Immutable Borrows");
+    // Task 1 Goals
+    //   a) Make multiple borrows of an object
+    //   b) Print the items you've borrowed
+    //   c) Modify the borrowed object
+    //   d) Print the borrowed object and the original object
+    // Hint:
+    //   Cannot borrow for writing if a reader is in-use
+    //   Cannot modify something that is not specified as modifiable
 
-struct Sensor(i32);
+    struct Sensor(i32);
+    let mut object = Sensor(111);
 
-impl Sensor {
-    fn read(&self) -> i32 {
-        self.0
+    let reader1 = &object;
+    let reader2 = &object;
+
+    println!("Reader values: {} and {}", reader1.0, reader2.0);
+    let writer = &mut object;
+    writer.0 += 1000;
+    println!("Writer value: {}", writer.0);
+    println!("Original value: {}", object.0);
+}
+
+fn task_2() {
+    println!("Task 2 - Function Borrows");
+    // Task 2 Goals
+    //   a) Pass an object to a read-only function
+    //   b) Pass object to a function that changes the parameter
+    //   d) Print the original object
+    // Hint:
+    //   Parameters must specify if they can be modified
+    //   Function cannot modify object if a reader is in-use
+
+    struct Sensor(i32);
+    fn reader(param: &Sensor) {
+        println!("Reader param: {}", param.0)
     }
-    fn calibrate(&mut self) {
-        self.0 *= 10;
+    fn writer(param: &mut Sensor) {
+        param.0 += 2000;
+        println!("Writer param: {}", param.0);
     }
+    let mut object = Sensor(222);
+    reader(&object);
+    writer(&mut object);
+    println!("Original value: {}", object.0);
 }
 
-fn read(device: &Sensor) {
-    println!("Read: {}", device.0);
+fn task_3() {
+    println!("Task 3 - Overlapping Borrows");
+    // Task 3 Goals
+    //   a) Borrow an object for printing a value
+    //   b) Pass object to a function that changes the parameter
+    //   d) Print the original object
+    // Hint:
+    //   Parameters must specify if they can be modified
+
+    struct Sensor(i32);
+    fn writer(param: &mut Sensor) {
+        param.0 += 3000;
+        println!("Writer param: {}", param.0);
+    }
+    let mut object = Sensor(333);
+    let reader = &object;
+    println!("Reader value: {}", reader.0);
+    writer(&mut object);
+    println!("Original value: {}", object.0);
 }
 
-fn calibrate(device: &mut Sensor) {
-    device.0 += 10;
+fn task_4() {
+    println!("Task 4 - Multiple Mutable Borrows");
+    // Task 4 Goals
+    //    a) 'sync_sensors' should copy contents of one sensor to the other
+    //    b) Print the values before and after
+    // Hint:
+    //    Not actually allowed!
+    //    Modify 'sync_sensor' and the caller to get the job done
+    struct Sensor(i32);
+    fn sync_sensor(sensor1: &mut Sensor, sensor2: i32) {
+        sensor1.0 = sensor2 + 4000;
+    }
+    let mut object = Sensor(444);
+    println!("Original value: {}", object.0);
+
+    // The following is not allowed - requires multiple mutable borrows
+    // sync_sensor (&mut object, &mut object);
+
+    // The following is also not allowed - both the read-only and the
+    // writable are in-use at the same time!
+    // sync_sensor (&mut object, &object);
+
+    // Even the following is not allowed - we're trying to read
+    // while the writable reference is still active!
+    // sync_sensor (&mut object, object.0);
+
+    let temp = object.0;
+    sync_sensor(&mut object, temp);
+
+    println!("New value: {}", object.0);
 }
 
-fn sync_sensors(dest: &mut Sensor, src: i32) {
-    dest.0 = src;
+fn task_5() {
+    println!("Task 5 - Method Borrows");
+    // Task 5 Goals
+    //   a) Pass an object to a read-only method
+    //   b) Pass object to a method that changes the parameter
+    //   d) Print the original object
+    // Hint:
+    //   Parameter must indicate that the object is the parameter
+    //   Parameters must specify if they can be modified
+    //   Function cannot modify object if a reader is in-use
+    //
+    struct Sensor(i32);
+    impl Sensor {
+        fn read(&self) -> i32 {
+            self.0
+        }
+        fn calibrate(&mut self) {
+            self.0 += 5000;
+        }
+    }
+    let mut object = Sensor(555);
+    println!("Original value: {}", object.read());
+    object.calibrate();
+    println!("new value: {}", object.read());
 }
 
-fn main() {
-    // TASK 1 - Use functions to print and modify the object
-    //          print and modify the value
-    // Hint: functions must specify writability
-    println!("Task 1");
-    let mut scanner1 = Sensor(101);
-    read(&scanner1);
-    calibrate(&mut scanner1);
-    read(&scanner1);
-
-    // TASK 2 - Use mutable and immutable local borrowers to
-    //          print and modify the value
-    // Hint: Cannot borrow something to modify if it's already borrowed
-    println!("Task 2");
-    let mut scanner2 = Sensor(42);
-    let reader2 = &scanner2;
-    println!("Reader: {}", reader2.0);
-    let writer2 = &mut scanner2;
-    writer2.0 *= 11;
-    println!("Writer {}", writer2.0);
-    let reader2 = &scanner2;
-    println!("Reader: {}", reader2.0);
-
-    // TASK 3 - Use both function and local borrowers to
-    //          print and modify the value
-    // Hint: Combining tasks 1 and 2!
-    println!("Task 3");
-    let mut scanner3 = Sensor(42);
-    let reader3 = &scanner3;
-    read(&scanner3);
-    println!("Reader: {}", reader3.0);
-    let writer3 = &mut scanner3;
-    writer3.0 *= 101;
-    println!("Writer {}", writer3.0);
-    calibrate(&mut scanner3);
-    let reader3 = &scanner3;
-    println!("Reader: {}", reader3.0);
-
-    // TASK 4 - Multiple mutable (and immutable) borrows via parameters
-    // Hint: Not allowed - do it another way (modify 'sync_sensors' as well)
-    println!("Task 4");
-    let mut scanner4 = Sensor(100);
-    read(&scanner4);
-
-    // NONE OF THESE WORK!
-
-    //sync_sensors (&mut scanner4, &mut scanner4);
-    // Wrong: simultaneous mutable borrows
-
-    //sync_sensors (&mut scanner4, scanner4);
-    // Wrong: mutable borrow while immutable borrow active
-
-    //sync_sensors (&mut scanner4, scanner4.0);
-    // Wrong: This is still a mutable borrow!
-
-    // Copy the source
-    let temp = scanner4.0;
-    sync_sensors(&mut scanner4, temp);
-    scanner4.0 = 0;
-    read(&scanner4);
-
-    // TASK 5 - Borrows in methods
-    // Hint: Use 'self' to refer to object but same conflict rules apply
-    println!("Task 5");
-    let mut scanner5 = Sensor(123);
-    let mut reader = &scanner5;
-    println!("Reader: {}", reader.read());
-    scanner5.calibrate();
-    reader = &scanner5;
-    println!("Reader: {}", reader.0);
-
-    // TASK 6 - Interior Mutability - increment counter every time
-    //          you read the sensor
-    // Hint: Use "Cell" (and it's accessor functions) to increment
-    //       counter on read, and print when queried.
-    //       (Don't forget to import "Cell")
-    println!("Task 6");
-
-    struct SensorWithCount {
+fn task_6() {
+    println!("Task 6 - Cell<T>");
+    // Task 6 Goals
+    //   a) Increment counter every time object is read
+    //   b) Reader should not be modifying the contents, just the counter
+    //   c) See what happens with multiple reader references!
+    // Hint:
+    //   Use "Cell" reference type to implement the counter
+    //   Do not forget to import the appropriate crate
+    use std::cell::Cell;
+    struct Sensor {
         data: i32,
         count: Cell<u32>,
     }
-
-    impl SensorWithCount {
+    impl Sensor {
         fn read(&self) -> i32 {
             self.count.set(self.count.get() + 1);
             self.data
         }
     }
-
-    let scanner6 = SensorWithCount {
+    let object = Sensor {
         data: 42,
         count: Cell::new(0),
     };
-    println!("Reads: {}", scanner6.count.get());
-    scanner6.read();
-    println!("Reads: {}", scanner6.count.get());
-    scanner6.read();
-    scanner6.read();
-    println!("Reads: {}", scanner6.count.get());
+    println!(
+        "Object: {} read {} times",
+        object.read(),
+        object.count.get()
+    );
+    let reader1 = &object;
+    let reader2 = &object;
+    println!(
+        "Reader1: {} read {} times",
+        reader1.read(),
+        reader1.count.get()
+    );
+    println!(
+        "Reader2: {} read {} times",
+        reader2.read(),
+        reader2.count.get()
+    );
+    println!(
+        "Reader1: {} read {} times",
+        reader1.read(),
+        reader1.count.get()
+    );
+    println!(
+        "Object: {} read {} times",
+        object.read(),
+        object.count.get()
+    );
+}
 
-    // TASK 7 - Implement a sensor history
-    // Hint: Use RefCell and its member functions to read/modify the history
-    println!("Task 7");
+fn task_7() {
+    println!("Task 7 - RefCell<T>");
+    // Task 7 Goals
+    //   a) Implement a sensor history
+    //   b) Make sure you can read and modify the history data
+    // Hint:
+    //   Use "RefCell" reference type to implement the counter
+    //   Do not forget to import the appropriate crate
+    //   RefCell borrow failures are run-time problems, not compile errors
+    use std::cell::RefCell;
 
     struct History {
         data: RefCell<Vec<u32>>,
@@ -141,6 +201,11 @@ fn main() {
     let values = History {
         data: RefCell::new(vec![1, 2, 3]),
     };
+
+    { // without the block scopes, run-time cannot tell if borrow is in-use
+        let immutable = values.data.borrow();
+        println!("Immutable values: {:?}", immutable);
+    }
 
     {
         let mut mutable = values.data.borrow_mut();
@@ -150,6 +215,19 @@ fn main() {
         mutable.push(5);
     }
 
-    let immutable = values.data.borrow();
-    println!("Values: {:?}", immutable);
+    {
+        let immutable = values.data.borrow();
+        println!("Values: {:?}", immutable);
+    }
+
+}
+
+fn main() {
+    task_1();
+    task_2();
+    task_3();
+    task_4();
+    task_5();
+    task_6();
+    task_7();
 }
--- courses/rust_essentials/150_borrowing/lab/prompt/src/main.rs
+++ courses/rust_essentials/150_borrowing/lab/prompt/src/main.rs
@@ -1,136 +1,193 @@
-//! Lab (prompt)
+//! Lab (answers)
 //! Borrowing
 //!
 //! Fix all the compile errors below by following the hints provided
 //!
 
-struct Sensor(i32);
+fn task_1() {
+    println!("Task 1 - Mutable and Immutable Borrows");
+    // Task 1 Goals
+    //   a) Make multiple borrows of an object
+    //   b) Print the items you've borrowed
+    //   c) Modify the borrowed object
+    //   d) Print the borrowed object and the original object
+    // Hint:
+    //   Cannot borrow for writing if a reader is in-use
+    //   Cannot modify something that is not specified as modifiable
 
-impl Sensor {
-    fn read(&self) -> i32 {
-        self.0
+    struct Sensor(i32);
+    let object = Sensor(111);
+
+    let reader1 = &object;
+    let reader2 = &object;
+    let writer = &mut object;
+    writer.0 += 1000;
+    println!("Reader values: {} and {}", reader1.0, reader2.0);
+    println!("Writer value: {}", writer.0)
+}
+
+fn task_2() {
+    println!("Task 2 - Function Borrows");
+    // Task 2 Goals
+    //   a) Pass an object to a read-only function
+    //   b) Pass object to a function that changes the parameter
+    //   d) Print the original object
+    // Hint:
+    //   Parameters must specify if they can be modified
+
+    struct Sensor(i32);
+    fn reader(param: Sensor) {
+        println!("Reader param: {}", param.0)
     }
-    fn calibrate(&mut self) {
-        self.0 *= 10;
+    fn writer(param: Sensor) {
+        param.0 += 2000;
+        println!("Writer param: {}", param.0);
     }
+    let object = Sensor(222);
+    reader(object);
+    writer(object);
+    println!("Original value: {}", object.0);
 }
 
-fn read(device: Sensor) {
-    println!("Read: {}", device.0);
+fn task_3() {
+    println!("Task 3 - Overlapping Borrows");
+    // Task 3 Goals
+    //   a) Borrow an object for printing a value
+    //   b) Pass object to a function that changes the parameter
+    //   d) Print the original object
+    // Hint:
+    //   Parameters must specify if they can be modified
+
+    struct Sensor(i32);
+    fn writer(param: mut Sensor) {
+        param.0 += 3000;
+        println!("Writer param: {}", param.0);
+    }
+    let mut object = Sensor(333);
+    let reader = &object;
+    writer(&object);
+    println!("Original value: {}", object.0);
+    println!("Reader value: {}", reader.0);
 }
 
-fn calibrate(device: Sensor) {
-    device.0 += 10;
+fn task_4() {
+    println!("Task 4 - Multiple Mutable Borrows");
+    // Task 4 Goals
+    //    a) 'sync_sensors' should copy contents of one sensor to the other
+    //    b) Print the values before and after
+    // Hint:
+    //    Not actually allowed!
+    //    Modify 'sync_sensor' and the caller to get the job done
+    struct Sensor(i32);
+    fn sync_sensor(sensor1: &mut Sensor, sensor2: &mut Sensor) {
+        sensor1.0 = sensor2.0 + 4000;
+    }
+    let mut object = Sensor(444);
+    println!("Original value: {}", object.0);
+    sync_sensor (&mut object, &mut object);
+    println!("New value: {}", object.0);
 }
 
-fn sync_sensors(dest: &mut Sensor, src: &mut Sensor) {
-    dest.0 = src.0;
-    src.0 = 0;
+fn task_5() {
+    println!("Task 5 - Method Borrows");
+    // Task 5 Goals
+    //   a) Pass an object to a read-only method
+    //   b) Pass object to a method that changes the parameter
+    //   d) Print the original object
+    // Hint:
+    //   Parameter must indicate that the object is the parameter
+    //   Parameters must specify if they can be modified
+    //   Function cannot modify object if a reader is in-use
+    //   
+    struct Sensor(i32);
+    impl Sensor {
+        fn read(param: &Sensor) -> i32 {
+            param.0
+        }
+        fn calibrate(param : &mut Sensor) {
+            param.0 += 5000;
+        }
+    }
+    let mut object = Sensor(555);
+    println!("Original value: {}", object.read());
+    object.calibrate();
+    println!("new value: {}", object.read());  
 }
 
-fn main() {
-
-    // TASK 1 - Use functions to print and modify the object
-    //          print and modify the value
-    // Hint: Cannot borrow something to modify if it's already borrowed
-    println!("Task 1");
-    let scanner1 = Sensor(101);
-    read(scanner1);
-    calibrate(scanner1);
-    read(scanner1);
-
-    // TASK 2 - Use mutable and immutable local borrowers to
-    //          print and modify the value
-    // Hint: Cannot borrow something to modify if it's already borrowed
-    println!("Task 2");
-    let scanner2 = Sensor(42);
-    let reader2 = &scanner2;
-    let writer2 = &mut scanner2;
-    writer2.0 *= 11;
-    println!("Reader: {}", reader2.0);
-    println!("Writer {}", writer2.0);
-    reader2 = &scanner2;
-    println!("Reader: {}", reader2.0);
-
-    // TASK 3 - Use both function and local borrowers to
-    //          print and modify the value
-    // Hint: Combining tasks 1 and 2!
-    println!("Task 3");
-    let mut scanner3 = Sensor(42);
-    let reader3 = &scanner3;
-    println!("Reader: {}", reader3.0);
-    read(&scanner3);
-    let writer3 = &mut scanner3;
-    calibrate(&mut scanner3);
-    writer3.0 *= 101;
-    let reader3 = &scanner3;
-    println!("Writer {}", writer3.0);
-    println!("Reader: {}", reader3.0);
-
-    // TASK 4 - Multiple mutable (and immutable) borrows via parameters
-    // Hint: Not allowed - do it another way (modify 'sync_sensors' as well)
-    println!("Task 4");
-    let mut scanner4 = Sensor(100);
-    read(&scanner4);
-    sync_sensors(&mut scanner4, &mut scanner4);
-    read(&scanner4);
-
-    // TASK 5 - Borrows in methods
-    // Hint: Use 'self' to refer to object but same conflict rules apply
-    println!("Task 5");
-    let mut scanner5 = Sensor(123);
-    let reader = &scanner5;
-    println!("Reader: {}", reader.read());
-    scanner5.calibrate();
-    println!("Reader: {}", reader.read());
-
-
-    // TASK 6 - Interior Mutability - increment counter every time
-    //          you read the sensor
-    // Hint: Use "Cell" (and it's accessor functions) to increment
-    //       counter on read, and print when queried
-    println!("Task 6");
-
-    struct SensorWithCount{
+fn task_6() {
+    println!("Task 6 - Cell<T>");
+    // Task 6 Goals
+    //   a) Increment counter every time object is read
+    //   b) Reader should not be modifying the contents, just the counter
+    //   c) See what happens with multiple reader references!
+    // Hint:
+    //   Use "Cell" reference type to implement the counter
+    //   Do not forget to import the appropriate crate
+    struct Sensor {
         data: i32,
-        count: i32,
+        count: u32,
     }
-
-    impl SensorWithCount {
+    impl Sensor {
         fn read(&self) -> i32 {
-            self.count = self.count + 1;
+            self.count += 1;
             self.data
         }
     }
-
-    let scanner6 = SensorWithCount {
+    let object = Sensor {
         data: 42,
         count: 0,
     };
-    println!("Reads: {}", scanner6.count);
-    scanner6.read();
-    println!("Reads: {}", scanner6.count);
-    scanner6.read();
-    scanner6.read();
-    println!("Reads: {}", scanner6.count);
+    println!(
+        "Object: {} read {} times",
+        object.read(),
+        object.count
+    );
+    let reader1 = &object;
+    let reader2 = &object;
+    println!(
+        "Reader1: {} read {} times",
+        reader1.read(),
+        reader1.count
+    );
 
-    // TASK 7 - Implement a sensor history
-    // Hint: Use RefCell and its member functions to read/modify the history
-    // Hint: RefCell Invalid borrows are runtime failures, not compile errors
-    println!("Task 7");
+}
 
-    struct History{
-        data: Vec<u32>,
+fn task_7() {
+    println!("Task 7 - RefCell<T>");
+    // Task 7 Goals
+    //   a) Implement a sensor history
+    //   b) Make sure you can read and modify the history data
+    // Hint:
+    //   Use "RefCell" reference type to implement the counter
+    //   Do not forget to import the appropriate crate
+    //   RefCell borrow failures are run-time problems, not compile errors
+
+    struct History {
+        data: RefCell<Vec<u32>>,
     }
     let values = History {
-        data: vec![1, 2, 3],
-    }
-    let mutable = &mut values;
-    println!("Values: {:?}", mutable.data);
-    mutable.data.push(4);
-    println!("Values: {:?}", mutable.data);
-    mutable.data.push(5);
+        data: RefCell::new(vec![1, 2, 3]),
+    };
+
     let immutable = values.data.borrow();
-    println!("Values: {:?}", *immutable);
-  
+    println!("Immutable values: {:?}", immutable);
+
+    let mut mutable = values.data.borrow_mut();
+    println!("Values: {:?}", mutable);
+    mutable.push(4);
+    println!("Values: {:?}", mutable);
+    mutable.push(5);
+
+    let immutable = values.data.borrow();
+    println!("Values: {:?}", immutable);
+}
+
+fn main() {
+    task_1();
+    task_2();
+    task_3();
+    task_4();
+    task_5();
+    task_6();
+    task_7();
 }
