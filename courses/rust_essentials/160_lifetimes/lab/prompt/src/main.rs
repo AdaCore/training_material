177446a698e4761f0ebbbc9fc13a3651eac81b7b
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2026-04-21 00:02:34 -0400

    Better lab presentation

--- courses/rust_essentials/160_lifetimes/lab/prompt/src/main.rs
+++ courses/rust_essentials/160_lifetimes/lab/prompt/src/main.rs
@@ -4,30 +4,14 @@
 //! Fix all the compile errors below by following the hints provided
 //!
 
-struct Owner {
-    name: String,
-}
-struct User<'a> {
-    name: &'a str,
-}
-
-fn choose<>(left: &str, right: &str) -> &str {
-    // Return reference to longer of 'left' or 'right'
-    if left.len() > right.len() {
-        left
-    } else {
-        right
-    }
-}
-
-fn find_user(owner: &Owner, _extra_data: &str) -> User {
-    User { name: &owner.name }
-
-fn main() {
-
-    // TASK 1 - Ensure reference is valid
-    // Hint: Cannot point to something that no longer exists
-    println!("Task 1");
+fn task_1() {
+    println!("Task 1 - Lifetime");
+    // Task 1 Goals
+    //   a) Create an object and a reference
+    //   b) Ensure object lives as long as reference
+    //   c) Print referenced value
+    // Hint:
+    //   Object should be in same scope as reference (or higher)
     let treasure_map: &i32;
     {
         let gold = 1000;
@@ -35,60 +19,118 @@ fn main() {
         println!("{gold}");
     }
     println!("{treasure_map}");
+}
 
-    // TASK 2 - Print the longer of two strings
-    // Hint: Make sure both borrowed objects live long enough
-    println!("Task 2");
-    let longer: &str;
-    {
-        let femur = String::from("the leg bone");
-        let ulna = String::from("an arm bone");
-        longer = choose(femur, ulna);
+fn task_2() {
+    println!("Task 2 - Solving ambiguity");
+    // Task 2 Goals
+    //   a) Create two String objects
+    //   b) Create a function that takes two 'str' references
+    //   c) Function should return longer of the two parameters
+    //   d) Print the function return value
+    // Hint:
+    //   Return value needs to live as long as longest-lived string
+
+    fn choose(left: &str, right: &str) -> &str {
+        // Return reference to longer of 'left' or 'right'
+        if left.len() > right.len() {
+            left
+        } else {
+            right
+        }
     }
-    println!("Longer bone is {longer}");
 
-    // TASK 3 - Prevent "use after free" bug
-    // Hint: "find_user" returns borrowed data - where does it come from?
-    println!("Task 3");
+    let longer12: &str;
+    let longer23: &str;
+    let longer13: &str;
+    let input1 = String::from("medium");
+    {
+        let input2 = String::from("short");
+        {
+            let input3 = String::from("very long");
+            longer12 = choose(&input1, &input2);
+            longer23 = choose(&input2, &input3);
+            longer13 = choose(&input1, &input3);
+        }
+    }
+    println!("longer12 = {}", longer12);
+    println!("longer23 = {}", longer23);
+    println!("longer13 = {}", longer13);
+}
+
+fn task_3() {
+    println!("Task 3 - Lifetimes in structs");
+    // Task 3 Goals
+    //   a) Create a function that returns a reference from within a struct
+    //   b) Print the returned data
+    // Hint:
+    //   Function needs to indicate where the borrowed data comes from
+    //   Struct needs to specify that the referenced element lasts as long as the struct
+
+    struct Owner {
+        name: String,
+    }
+    struct User {
+        name: &str,
+    }
+
+    fn find_user(owner: &Owner, _extra_data: &str) -> User {
+       User { name: &owner.name }
+    }
+
     let my_owner = Owner {
-        name: String::from("Permanent")
+        name: String::from("Permanent"),
     };
     let result_user;
     {
         let short_lived_str = String::from("Temporary");
-        result_user = find_user(my_owner, short_lived_str);
-    } 
+        result_user = find_user(&my_owner, &short_lived_str);
+    }
     println!("{}", result_user.name);
+}
 
-    // TASK 4 - Share the scroll among partners
-    // Hint: When you borrow something, ensure what you borrowed lives long enough
-    println!("Task 4");
-    struct OwnedScroll {
-        inscription: String,
+fn task_4() {
+    println!("Task 4 - Lifetimes for enums");
+    // Task 4 Goals
+    //   a) Create a function that returns a reference from within an enum variant
+    //   b) Print the returned data
+    // Hint:
+    //   Function needs to indicate where the borrowed data comes from
+    //   Enum needs to specify that the referenced variant lasts as long as the enum
+    struct Owner {
+        name: String,
     }
 
-    struct BorrowedScroll {
-        inscription: &str,
+    enum UserSession {
+        _Admin,
+        Guest(&str),
     }
 
-    let orc: BorrowedScroll;
-    let elf: BorrowedScroll;
+    fn get_session(owner: &Owner, _extra: &str) -> UserSession {
+        UserSession::Guest(&owner.name)
+    }
 
+    let my_owner = Owner {
+        name: String::from("Permanent"),
+    };
+
+    let result_session;
     {
-        let leader = OwnedScroll {
-            inscription: String::from("Avada Kedavera"),
-        };
-        println!("Leader's scroll: {}", leader.inscription);
-
-        orc = BorrowedScroll {
-            inscription: &leader.inscription,
-        };
-        elf = BorrowedScroll {
-            inscription: &leader.inscription,
-        };
+        let short_lived_str = String::from("Temporary");
+        result_session = get_session(&my_owner, &short_lived_str);
     }
 
-    println!("Orc is looking at: {}", orc.inscription);
-    println!("Elf is looking at: {}", elf.inscription);
+    match result_session {
+        UserSession::Guest(name) => println!("Guest name: {}", name),
+        UserSession::_Admin => println!("Logged in as Admin"),
+    }
 
 }
+
+fn main() {
+    task_1();
+    task_2();
+    task_3();
+    task_4();
+}
+
