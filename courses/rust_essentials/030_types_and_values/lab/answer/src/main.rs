ee90f57fd0b6642702a08fac141eb123b1303939
Julien Berkane~ <berkane@adacore.com>
2026-04-24 11:13:56 +0200

    [!] Third Push

--- courses/rust_essentials/030_types_and_values/lab/answer/src/main.rs
+++ courses/rust_essentials/030_types_and_values/lab/answer/src/main.rs
@@ -13,26 +13,22 @@ fn main() {
     let apples = 5;
     let person = "Alice"; 
     
-
     // TASK 2 - Mutable Variable
     // Hint: Keyword "mut" makes a variable mutable. Add a declaration to make 'money' mutable
     let mut money = 10;        
     money = money + 20; 
 	
-
     // TASK 3 - Variable Types
     // Hint: Initial value must match the types  
     let initial : char = 'a';
     let big_number : u32 = 150000;  
    
-
     // TASK 4 - Type Inference and Static Type
     // Hint: Type is inferred by initial value. Numeric literals must match the type assigned
 	let mut my_float = 6.5;
     my_float = 5.0;
     let mut my_int = 18;
     my_int += 7;  
-
 
     // TASK 5 - Base Numeric Literal
     // Hint: Convert the value into different bases and verify the display
@@ -45,21 +41,18 @@ fn main() {
     println!("TASK5 => decimal    : {}", decimal);
     println!("TASK5 => octal      : {}", octal);
     println!("TASK5 => hexadecimal: {}", hexadecimal);
-    
-    
+       
     // TASK 6 - Numeric Conversion
     // Hint: Keyword "as" tells the compiler to interpret a value as a different type  
     let my_int_2: i32 = 10;
     let my_float_2: f64 = 5.5;
     let sum = my_int_2 as f64 + my_float_2; 
     
-
     // TASK 7 - Arithmetic and the Exponent Trap
     // Hint: Code compiles but the value is not correct 
     let exponent = 5_i32.pow(2);
     println!("TASK7 => 5 to the power of 2 equals: {}", exponent);
     
-
     // TASK 8 - Arithmetic Division
     // Code compiles but the value is not correct
     // How do we print the decimal value here?
