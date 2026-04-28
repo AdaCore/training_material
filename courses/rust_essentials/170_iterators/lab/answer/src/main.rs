cc0a96327c108fcb283ac5f5f86e21e237154a23
Julien Berkane <berkane@adacore.com>
2026-04-28 08:22:42 +0000

    Apply 3 suggestion(s) to 1 file(s)
    Co-authored-by: Dana Binkley <binkley@adacore.com>

--- courses/rust_essentials/170_iterators/lab/answer/src/main.rs
+++ courses/rust_essentials/170_iterators/lab/answer/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     }
     
     // TASK 2 - Using Our Iterator
-    // Hint: The 'for' loop calls .next() under the hood
+    // Hint: The 'for' loop calls '.next()' under the hood
     struct SliceIter<'s> {
         slice: &'s [i32],
         idx: usize,
@@ -43,7 +43,7 @@ fn main() {
 
     // TASK 3 - Getting an Iterator - 'iter()'
     // Hint: Collections themselves are not iterators
-    // Using iter() does not consume the collection
+    // Using 'iter()' does not consume the collection
     let numbers_2 = vec![10, 20, 30];
         for n in numbers_2.iter() {
         println!("{n}");
@@ -70,7 +70,7 @@ fn main() {
         println!("This value is divisible by 2: {elem}");
     }
     
-    // TASK 5 - Common consummers
+    // TASK 5 - Common Consumers
     // Hint: 'sum' adds all values and return a single value, 'any' returns True if any value matches condition
     fn is_freezing(temp: &i32) -> bool {
         *temp < 0
