d3cc3a6ce270f5e26a871e16e3e30eacd6a9d9eb
Julien Berkane <berkane@adacore.com>
2026-04-30 13:55:11 +0000

    Resolve "Create Lab for Module: Types and values"

--- courses/rust_essentials/030_types_and_values/lab/answer/src/main.rs
+++ courses/rust_essentials/030_types_and_values/lab/answer/src/main.rs
@@ -1,3 +1,66 @@
+//! Lab (answer)
+//! Types and Values
+//!
+//! Fix all the compile errors below and/or follow the hints provided
+//!
+
+#![allow(unused_assignments)]
+#![allow(unused_variables)]
+
 fn main() {
-    println!("TBD");
+    // TASK 1 - Variable Declaration
+    // Hint: Variable declaration has a particular syntax using ":" for types and "=" for value and ending with a ";"
+    let apples = 5;
+    let person = "Alice"; 
+    
+    // TASK 2 - Mutable Variable
+    // Hint: Keyword "mut" makes a variable mutable. Add a declaration to make 'money' mutable
+    let mut money = 10;        
+    money = money + 20; 
+	
+    // TASK 3 - Variable Types
+    // Hint: Initial value must match the types  
+    let initial : char = 'a';
+    let big_number : u32 = 150000;  
+   
+    // TASK 4 - Type Inference and Static Type
+    // Hint: Type is inferred by initial value. Numeric literals must match the type assigned
+	let mut my_float = 6.5;
+    my_float = 5.0;
+    let mut my_int = 18;
+    my_int += 7;  
+
+    // TASK 5 - Base Numeric Literal
+    // Hint: Convert the value into different bases and verify the display
+    let binary = 0b1111_0000;
+    let decimal = 240;
+    let octal = 0o360;
+    let hexadecimal = 0xf0;
+
+    println!("TASK5 => binary     : {}", binary);
+    println!("TASK5 => decimal    : {}", decimal);
+    println!("TASK5 => octal      : {}", octal);
+    println!("TASK5 => hexadecimal: {}", hexadecimal);
+       
+    // TASK 6 - Numeric Conversion
+    // Hint: Keyword "as" tells the compiler to interpret a value as a different type  
+    let my_int_2: i32 = 10;
+    let my_float_2: f64 = 5.5;
+    let sum = my_int_2 as f64 + my_float_2; 
+    
+    // TASK 7 - Arithmetic and the Exponent Trap
+    // Hint: Code compiles but the value is not correct 
+    let exponent = 5_i32.pow(2);
+    println!("TASK7 => 5 to the power of 2 equals: {}", exponent);
+    
+    // TASK 8 - Arithmetic Division
+    // Code compiles but the value is not correct
+    // How do we print the decimal value here?
+    // Hint: Remember division behavior depends on the type
+    let precise = 7.0 / 3.0;
+    println!("TASK8 => 7 divided by 3 equals: {}", precise);
+    // Using float literals for this division is too much
+    // How do we get an integer result here?
+    let too_precise = 8 / 2;
+    println!("TASK8 => 8 divided by 2 equals: {:?}", too_precise);    
 }
