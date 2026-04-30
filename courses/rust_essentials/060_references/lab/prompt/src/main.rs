3a451c8debc000a987636e5bb9aab5ddcd21fb71
Michael Frank <frank@adacore.com>
2026-04-30 13:12:04 +0000

    Resolve "Create Lab for Module: References"

--- courses/rust_essentials/060_references/lab/prompt/src/main.rs
+++ courses/rust_essentials/060_references/lab/prompt/src/main.rs
@@ -1,3 +1,115 @@
+//! Lab (prompt)
+//! References
+//!
+//! Fix all the compile errors below by following the hints provided
+//!
+
 fn main() {
-    println!("TBD");
+    struct ReferencesStruct {
+        count: i8,
+        value: f64,
+    }
+
+    // Task 1 Goal
+    //   Use a reference to show changes in the pointed-to value
+    // Hint
+    //   Cannot modify a shared reference directly
+    println!("Task 1 - Observer");
+    let mut angle = ReferencesStruct {
+        count: 12,
+        value: 34.5,
+    };
+    let ref_angle = &angle;
+    println!("Before: {} {}", ref_angle.count, ref_angle.value);
+    ref_angle.count *= 2;
+    ref_angle.value *= 2.0;
+    println!("After: {} {}", ref_angle.count, ref_angle.value);
+
+    // Task 2 Goal
+    //   Use a single reference to print the values of multiple pointed-to objects
+    // Hint
+    //   A shared reference can be mutable!
+    println!("Task 2 - Rebinder");
+    let bricks = ReferencesStruct {
+        count: 6,
+        value: 78.9,
+    };
+    let buildings = ReferencesStruct {
+        count: 1,
+        value: 2.3,
+    };
+    let ref_both = &bricks;
+    println!("Bricks: {} {}", ref_both.count, ref_both.value);
+    ref_both = &buildings;
+    println!("Buildings: {} {}", ref_both.count, ref_both.value);
+
+    // Task 3 Goal
+    //   Use a reference to change the value of a pointed-to object
+    // Hint
+    //   The original object must be modifiable
+    //   The reference object must allow for modification of the contents
+    println!("Task 3 - Modifier");
+    let cars = ReferencesStruct {
+        count: 67,
+        value: 8.9,
+    };
+    println!("Before: {} {}", cars.count, cars.value);
+    let ref_cars = &cars;
+    ref_cars.count = ref_cars.count / 2;
+    ref_cars.value = ref_cars.value / 2.0;
+    println!("After: {} {}", cars.count, cars.value);
+
+    // Task 4 Goal
+    //   Create a single reference object to modify multiple pointed-to objects
+    // Hint
+    //   Both the variable holding the reference AND the reference itself must be mutable
+    println!("Task 4 - Free Agent");
+    let dog = ReferencesStruct {
+        count: 1,
+        value: 2.3,
+    };
+    let duck = ReferencesStruct {
+        count: 4,
+        value: 5.6,
+    };
+    let ref_both: &ReferencesStruct = &dog;
+    println!("Before dog: {} {}", ref_both.count, ref_both.value);
+    *ref_both = ReferencesStruct {
+        count: 0,
+        value: 0.0,
+    };
+    println!("After dog: {} {}", ref_both.count, ref_both.value);
+
+    ref_both = &duck;
+    println!("Before duck: {} {}", ref_both.count, ref_both.value);
+    *ref_both = ReferencesStruct {
+        count: 0,
+        value: 0.0,
+    };
+    println!("After duck: {} {}", ref_both.count, ref_both.value);
+
+    // Task 5 Goal
+    //   Modify the existing code to print correctly
+    // Hint
+    //   Need to make the referenced object last longer
+    println!("Task 5 - Dangling Reference");
+    let rose = {
+        let jack = String::from("Jack");
+        &jack // 'rose' is a reference to 'jack'
+    };
+    println!("Jack screams '{rose}'");
+
+    // Task 6 Goal
+    //   Use a single reference object to print all different length slices of 'floats'
+    // Hint
+    //   Reference object refers to a slice of the object, not length
+    //   "{:?}" is used to print the contents of the array (Debug)
+    println!("Task 6 - Slices");
+    let floats: [f64; 5] = [1.1, 22.22, 333.333, 4444.4444, 55555.555555];
+    let mut ref_floats: &[f64] = &floats;
+    println!("{:?}", ref_floats);
+    ref_floats = &floats;
+    println!("{:?}", ref_floats);
+    ref_floats = &floats;
+    println!("{:?}", ref_floats);
 }
