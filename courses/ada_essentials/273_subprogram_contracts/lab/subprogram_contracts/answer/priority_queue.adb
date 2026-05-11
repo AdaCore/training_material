3d558265d3e9b073b0e28146a3115022ca91df9e
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2026-05-11 12:23:04 -0400

    Updating answer code for better lab experience

--- courses/ada_essentials/273_subprogram_contracts/lab/subprogram_contracts/answer/priority_queue.adb
+++ courses/ada_essentials/273_subprogram_contracts/lab/subprogram_contracts/answer/priority_queue.adb
@@ -1,35 +1,28 @@
 package body Priority_Queue is
 
-   function Pad (Str : String) return String_T is
-      Retval : String_T := (others => ' ');
+   procedure Push
+     (Queue : in out Queue_T;
+      Item  :        Item_T) is
+      Last : Size_T renames Queue.Size;
    begin
-      if Str'Length > Retval'Length then
-         Retval := Str (Str'First .. Str'First + Retval'Length - 1);
-      else
-         Retval (1 .. Str'Length) := Str;
-      end if;
-      return Retval;
-   end Pad;
-
-   procedure Push (Queue    : in out Queue_T;
-                   Priority :        Priority_T;
-                   Value    :        String) is
-      Last      : Size_T renames Queue.Size;
-      New_Entry : constant Entries_T := (Priority, Pad (Value));
-   begin
-      if Queue.Size = 0 then
-         Queue.Entries (Last + 1) := New_Entry;
-      elsif Priority < Queue.Entries (1).Priority then
+      if Queue.Size = 0
+      then
+         Queue.Entries (Last + 1) := Item;
+      elsif Item.Priority < Queue.Entries (1).Priority
+      then
          Queue.Entries (2 .. Last + 1) := Queue.Entries (1 .. Last);
-         Queue.Entries (1)             := New_Entry;
-      elsif Priority > Queue.Entries (Last).Priority then
-         Queue.Entries (Last + 1) := New_Entry;
+         Queue.Entries (1)             := Item;
+      elsif Item.Priority > Queue.Entries (Last).Priority
+      then
+         Queue.Entries (Last + 1) := Item;
       else
-         for Index in 1 .. Last loop
-            if Priority <= Queue.Entries (Index).Priority then
+         for Index in 1 .. Last
+         loop
+            if Item.Priority <= Queue.Entries (Index).Priority
+            then
                Queue.Entries (Index + 1 .. Last + 1) :=
                  Queue.Entries (Index .. Last);
-               Queue.Entries (Index) := New_Entry;
+               Queue.Entries (Index)                 := Item;
                exit;
             end if;
          end loop;
@@ -37,8 +30,9 @@ package body Priority_Queue is
       Last := Last + 1;
    end Push;
 
-   procedure Pop (Queue : in out Queue_T;
-                  Value :    out String_T) is
+   procedure Pop
+     (Queue : in out Queue_T;
+      Value :    out Value_T) is
    begin
       Value      := Queue.Entries (Queue.Size).Value;
       Queue.Size := Queue.Size - 1;
--- courses/ada_essentials/273_subprogram_contracts/lab/subprogram_contracts/answer/priority_queue.ads
+++ courses/ada_essentials/273_subprogram_contracts/lab/subprogram_contracts/answer/priority_queue.ads
@@ -1,41 +1,105 @@
 package Priority_Queue is
-   Overflow : exception;
+   Overflow       : exception;
+   Duplicate_Item : exception;
+
    type Priority_T is (Low, Medium, High);
-   type Queue_T is tagged private;
-   subtype String_T is String (1 .. 20);
-
-   procedure Push (Queue    : in out Queue_T;
-                   Priority :        Priority_T;
-                   Value    :        String) with
-     Pre => (not Full (Queue) and then Value'Length > 0) or else raise Overflow,
-     Post => Valid (Queue);
-   procedure Pop (Queue : in out Queue_T;
-                  Value :    out String_T) with
-     Pre => not Empty (Queue), Post => Valid (Queue);
-
-   function Full (Queue : Queue_T) return Boolean;
-   function Empty (Queue : Queue_T) return Boolean;
-   function Valid (Queue : Queue_T) return Boolean;
-private
-   Max_Queue_Size : constant := 10;
-   type Entries_T is record
+   type Value_T is new Integer;
+   type Item_T is record
       Priority : Priority_T;
-      Value    : String_T;
+      Value    : Value_T;
    end record;
+
+   type Queue_T is tagged private;
+
+   procedure Push
+     (Queue : in out Queue_T;
+      Item  :        Item_T) with
+     Pre  =>
+      (if Full (Queue) then raise Overflow)
+      and then
+      (if Contains (Queue, Item.Value) then
+         raise Duplicate_Item with Item.Value'Image),
+     Post =>
+      Length (Queue) = Length (Queue'Old) + 1
+      and then Is_Consistent (Queue'Old, Queue, Item.Value)
+      and then Is_Ordered (Queue);
+   procedure Pop
+     (Queue : in out Queue_T;
+      Value :    out Value_T) with
+     Pre  => not Empty (Queue),
+     Post =>
+      Length (Queue) = Length (Queue'Old) - 1
+      and then not (Contains (Queue, Value))
+      and then Is_Consistent (Queue, Queue'Old, Value)
+      and then Is_Ordered (Queue);
+
+   function Full
+     (Queue : Queue_T)
+      return Boolean;
+   function Empty
+     (Queue : Queue_T)
+      return Boolean;
+   function Length
+     (Queue : Queue_T)
+      return Integer;
+   function Is_Ordered
+     (Queue : Queue_T)
+      return Boolean;
+   function Contains
+     (Queue : Queue_T;
+      Value : Value_T)
+      return Boolean;
+   function Is_Consistent
+     (Before : Queue_T;
+      After  : Queue_T;
+      Value  : Value_T)
+      return Boolean;
+private
+   Max_Queue_Size : constant := 100;
    type Size_T is range 0 .. Max_Queue_Size;
-   type Queue_Array_T is array (1 .. Size_T'Last) of Entries_T;
+   type Queue_Array_T is array (1 .. Size_T'Last) of Item_T;
    type Queue_T is tagged record
       Size    : Size_T := 0;
       Entries : Queue_Array_T;
    end record;
 
-   function Full (Queue : Queue_T) return Boolean is (Queue.Size = Size_T'Last);
-   function Empty (Queue : Queue_T) return Boolean is (Queue.Size = 0);
+   function Length
+     (Queue : Queue_T)
+      return Integer is (Integer (Queue.Size));
 
-   function Valid (Queue : Queue_T) return Boolean is
+   function Full
+     (Queue : Queue_T)
+      return Boolean is (Queue.Size = Size_T'Last);
+   function Empty
+     (Queue : Queue_T)
+      return Boolean is (Queue.Size = 0);
+
+   function Is_Ordered
+     (Queue : Queue_T)
+      return Boolean is
      (if Queue.Size <= 1 then True
       else
         (for all Index in 2 .. Queue.Size =>
            Queue.Entries (Index).Priority >=
            Queue.Entries (Index - 1).Priority));
+
+   function Contains
+     (Queue : Queue_T;
+      Value : Value_T)
+      return Boolean is
+     (if Empty (Queue) then False
+      else
+        (for some Index in 1 .. Queue.Size =>
+           Queue.Entries (Index).Value = Value));
+
+   function Is_Consistent
+     (Before : Queue_T;
+      After  : Queue_T;
+      Value  : Value_T)
+      return Boolean is
+     (Contains (After, Value)
+      and then
+      (for all Index in 1 .. Before.Size =>
+         Contains (After, Before.Entries (Index).Value)));
+
 end Priority_Queue;
