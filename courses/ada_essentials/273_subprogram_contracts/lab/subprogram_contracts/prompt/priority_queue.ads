f9d4837489b8d3f78174ab02c608eca0d971cca5
Michael Frank <55284511+frank-at-adacore@users.noreply.github.com>
2026-05-11 15:59:08 -0400

    Make the prompt easier to follow

--- courses/ada_essentials/273_subprogram_contracts/lab/subprogram_contracts/prompt/priority_queue.ads
+++ courses/ada_essentials/273_subprogram_contracts/lab/subprogram_contracts/prompt/priority_queue.ads
@@ -1,19 +1,39 @@
 package Priority_Queue is
+   Overflow       : exception;
+   Duplicate_Item : exception;
+
    type Priority_T is (Low, Medium, High);
+   type Value_T is new Integer;
+   type Item_T is record
+      Priority : Priority_T;
+      Value    : Value_T;
+   end record;
+
    type Queue_T is tagged private;
 
-   procedure Push
-     (Queue    : in out Queue_T;
-      Priority :        Priority_T;
-      Value    :        String);
-      --  Add pre/post conditions as necessary
+   procedure Push (Queue : in out Queue_T;
+                   Item  :        Item_T);
+   procedure Pop (Queue : in out Queue_T;
+                  Value :    out Value_T);
 
-   procedure Pop
-     (Queue : in out Queue_T;
-      Value :    out String);
-      --  Add pre/post conditions as necessary
+   function Full (Queue : Queue_T) return Boolean;
+   function Empty (Queue : Queue_T) return Boolean;
+   function Length (Queue : Queue_T) return Integer;
+   function Is_Ordered (Queue : Queue_T) return Boolean;
+
 private
-   type Queue_T is tagged null record;
-   -- Implement the queue
+   Max_Queue_Size : constant := 100;
+   type Size_T is range 0 .. Max_Queue_Size;
+   type Queue_Array_T is array (1 .. Size_T'Last) of Item_T;
+   type Queue_T is tagged record
+      Size    : Size_T := 0;
+      Entries : Queue_Array_T;
+   end record;
+
+   --  Implement these expression functions correctly
+   function Length (Queue : Queue_T) return Integer is (0);
+   function Full (Queue : Queue_T) return Boolean is (True);
+   function Empty (Queue : Queue_T) return Boolean is (True);
+   function Is_Ordered (Queue : Queue_T) return Boolean is (True);
 
 end Priority_Queue;
